b1b5cc9c3933696a69f20a145b225642e5c5eccc
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-02-04 00:47:06 +0900

    feat(packages): Input 컴포넌트

---
+++ packages/ui/src/Input/ConditionalMessage.tsx
@@ -0,0 +1,46 @@
+import { color, font } from '@maru/design-system';
+import styled, { css } from 'styled-components';
+
+interface ConditionalMessageProps {
+  message?: string;
+  errorMessage?: string;
+  isError?: boolean;
+}
+
+const ConditionalMessage = ({
+  message,
+  errorMessage,
+  isError = false,
+}: ConditionalMessageProps) => {
+  return isError ? (
+    errorMessage ? (
+      <div style={{ position: 'relative' }}>
+        <StyledConditionalMessage $isError={true}>
+          {errorMessage}
+        </StyledConditionalMessage>
+      </div>
+    ) : null
+  ) : message ? (
+    <StyledConditionalMessage $isError={false}>{message}</StyledConditionalMessage>
+  ) : null;
+};
+
+export default ConditionalMessage;
+
+const StyledConditionalMessage = styled.p<{ $isError: boolean }>`
+  ${(props) =>
+    props.$isError
+      ? css`
+          position: absolute;
+          top: 0;
+          left: 0;
+          ${font.caption}
+          color: ${color.red};
+          margin-top: 8px;
+        `
+      : css`
+          ${font.p3};
+          color: ${color.gray500};
+          margin-top: 4px;
+        `}
+`;
