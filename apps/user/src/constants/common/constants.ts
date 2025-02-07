a47c88cec1b57fb51380d0b8ade8441ea29a24c6
junghongseop <a01082372487@gmail.com>
2025-02-07 20:38:13 +0900

    feat(user): 푸터, 헤더 개발하면서 필요했던 라우터 constants

---
+++ apps/user/src/constants/common/constants.ts
@@ -0,0 +1,11 @@
+export const ROUTES = {
+  MAIN: '/',
+  FORM: '/form',
+  FORM_MANAGEMENT: '/management',
+  NOTICE: '/notice',
+  FAQ: '/faq',
+  ADMISSION_REGISTRATION: '/enroll',
+  TERMS_OF_SERVICE: '/tos',
+  PRIVACY_POLCY: '/privacy',
+  PERSONAL_INFO_COLLECTION: '/collection',
+};
