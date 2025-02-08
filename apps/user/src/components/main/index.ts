0a5a58ae0cdc9cebcff87a2ed5cbe0205243114a
junghongseop <a01082372487@gmail.com>
2025-02-08 21:04:25 +0900

    feat(user): 입학전형 설명회 신청 박스 컴포넌트 개발

--- apps/user/src/components/main/index.ts
+++ apps/user/src/components/main/index.ts
@@ -1,2 +1,3 @@
 export { default as ScheduleList } from './ScheduleList/ScheduleList';
 export { default as GuidelineBox } from './GuidelineBox/GuidelineBox';
+export { default as ApplicationBox } from './ApplicationBox/ApplicationBox';
