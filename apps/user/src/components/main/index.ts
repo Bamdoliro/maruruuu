43bc035230231ab1acc4ea44229fae8a591fa232
junghongseop <a01082372487@gmail.com>
2025-02-08 21:18:13 +0900

    feat(user): 성적 모의 계산 상자 컴포넌트 개발

--- apps/user/src/components/main/index.ts
+++ apps/user/src/components/main/index.ts
@@ -1,3 +1,4 @@
 export { default as ScheduleList } from './ScheduleList/ScheduleList';
 export { default as GuidelineBox } from './GuidelineBox/GuidelineBox';
 export { default as ApplicationBox } from './ApplicationBox/ApplicationBox';
+export { default as SimulatorBox } from './SimulatorBox/SimulatorBox';
