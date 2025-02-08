c3a5bbd73b04d102f7a01caf469f4612c15d97fa
junghongseop <a01082372487@gmail.com>
2025-02-08 18:27:53 +0900

    feat(user): 스케줄 아이템 컴포넌트 개발

---
+++ apps/user/src/components/main/ScheduleList/ScheduleItem/ScheduleItem.tsx
@@ -0,0 +1,44 @@
+import { color } from '@maru/design-system';
+import { Text } from '@maru/ui';
+import { flex } from '@maru/utils';
+import styled from 'styled-components';
+import dayjs, { Dayjs } from 'dayjs';
+import isBetween from 'dayjs/plugin/isBetween';
+
+dayjs.extend(isBetween);
+
+interface ScheduleItemProps {
+  title: string;
+  date: string;
+  startTime: Dayjs;
+  endTime: Dayjs;
+}
+
+const ScheduleItem = ({ title, date, startTime, endTime }: ScheduleItemProps) => {
+  const now = dayjs();
+  const active = now.isBetween(startTime, endTime);
+
+  return (
+    <StyledScheduleItem isActive={active}>
+      <Text fontType="H5" color={active ? color.maruDefault : color.gray900}>
+        {title}
+      </Text>
+      <Text fontType="p2" color={color.gray700}>
+        {date}
+      </Text>
+    </StyledScheduleItem>
+  );
+};
+
+export default ScheduleItem;
+
+const StyledScheduleItem = styled.div<{ isActive: boolean }>`
+  ${flex({ alignItems: 'center', justifyContent: 'space-between' })}
+  width: 100%;
+  height: 64px;
+  flex-shrink: 0;
+  background-color: ${color.gray50};
+  border-radius: 12px;
+  padding: 0 20px;
+  border: ${(props) => (props.isActive ? `1px solid ${color.maruDefault}` : 'none')};
+`;
