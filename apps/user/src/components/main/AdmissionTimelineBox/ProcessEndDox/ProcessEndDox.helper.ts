d412accc8ee98d63db59d5d8b49b9e8931edc6d1
junghongseop <a01082372487@gmail.com>
2025-02-10 01:34:04 +0900

    feat(user): 입학 전형이 종료되면 보여주는 Box 컴포넌트 개발

---
+++ apps/user/src/components/main/AdmissionTimelineBox/ProcessEndDox/ProcessEndDox.helper.ts
@@ -0,0 +1,15 @@
+import dayjs from 'dayjs';
+import { SCHEDULE } from '@/constants/form/constants';
+
+export const processEnd = () => {
+  const today = dayjs().format('YYYY년 MM월 DD일 (ddd) HH:mm');
+  const startDate = SCHEDULE.입학_등록_마감.format('YYYY년 MM월 DD일 (ddd) HH:mm');
+
+  const now = dayjs().startOf('day');
+  const registrationEnd = SCHEDULE.입학_등록_마감.startOf('day');
+
+  const diff = now.diff(registrationEnd, 'day');
+  const overDay = `D+${diff}`;
+
+  return { today, startDate, overDay };
+};
