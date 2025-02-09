11f7259d4cd925f63fbcc6acd990b8910c7fa33b
junghongseop <a01082372487@gmail.com>
2025-02-10 01:41:35 +0900

    feat(user): AdmissionTimelineBox 컴포넌트 개발

--- apps/user/src/components/main/index.ts
+++ apps/user/src/components/main/index.ts
@@ -2,3 +2,4 @@ export { default as ScheduleList } from './ScheduleList/ScheduleList';
 export { default as GuidelineBox } from './GuidelineBox/GuidelineBox';
 export { default as ApplicationBox } from './ApplicationBox/ApplicationBox';
 export { default as SimulatorBox } from './SimulatorBox/SimulatorBox';
+export { default as AdmissionTimelineBox } from './AdmissionTimelineBox/AdmissionTimelineBox';
