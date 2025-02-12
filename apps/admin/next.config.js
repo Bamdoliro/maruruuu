4aae39620bdfb4604a61425b320f998f02751e65
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-02-12 20:03:55 +0900

    config(admin): eslint 및 config 설정

--- apps/admin/next.config.js
+++ apps/admin/next.config.js
@@ -1,4 +1,52 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  transpilePackages: ['@maru/design-system'],
+  poweredByHeader: false,
+  images: {
+    unoptimized: false,
+    domains: ['s3.ap-northeast-2.amazonaws.com'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: `${process.env.NEXT_PUBLIC_BASE_URL}`,
+      },
+    ],
+  },
+  reactStrictMode: true,
+  swcMinify: true,
+  // async rewrites() {
+  //   return [
+  //     {
+  //       source: '/:path*',
+  //       destination: `${process.env.NEXT_PUBLIC_BASE_URL}/:path*`,
+  //     },
+  //   ];
+  // },
+  async headers() {
+    return [
+      {
+        source: '/(.*)?',
+        headers: [
+          {
+            key: 'X-XSS-Protection',
+            value: '1; mode=block',
+          },
+          {
+            key: 'X-Frame-Options',
+            value: 'DENY',
+          },
+          {
+            key: 'X-Content-Type-Options',
+            value: 'nosniff',
+          },
+          {
+            key: 'X-DNS-Prefetch-Control',
+            value: 'on',
+          },
+        ],
+      },
+    ];
+  },
+};
 
-export default nextConfig;
+module.exports = nextConfig;
