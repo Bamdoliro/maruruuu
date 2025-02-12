f6f40cee3c88cf0f46be498f8e40e0a0cad910b4
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-02-12 20:02:13 +0900

    feat(admin): 리프레시 토큰 통한 Request 인터셉터 기능 추가

--- apps/admin/src/apis/instance/instance.ts
+++ apps/admin/src/apis/instance/instance.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { refreshToken } from '../token';
 
 export const maru = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -13,6 +14,27 @@ maru.interceptors.request.use(
     return config;
   },
   (error) => {
+    return Promise.reject(error);
+  }
+);
+
+maru.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  async (error) => {
+    if (error.response) {
+      const {
+        status,
+        data: { message, code },
+      } = error.response;
+
+      if (message) {
+        if (status === 401 && code === 'EXPIRED_TOKEN') {
+          refreshToken();
+        }
+      }
+    }
     return Promise.reject(error);
   }
 );
