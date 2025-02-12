5d5b1b6b70dbc910134d3e251580086165191e66
junghongseop <a01082372487@gmail.com>
2025-02-12 17:28:03 +0900

    feat(user): 공지사항 페이지 개발

---
+++ apps/user/src/app/notice/page.tsx
@@ -0,0 +1,37 @@
+'use client';
+
+import { NoticeList } from '@/components/notice';
+import AppLayout from '@/layouts/AppLayout';
+import { color } from '@maru/design-system';
+import { Loader, Text } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { Suspense } from 'react';
+import styled from 'styled-components';
+
+const Notice = () => {
+  return (
+    <AppLayout header footer>
+      <StyledNotice>
+        <Text fontType="H1" color={color.gray900}>
+          공지사항
+        </Text>
+        <Suspense fallback={<Loader />}>
+          <NoticeList />
+        </Suspense>
+      </StyledNotice>
+    </AppLayout>
+  );
+};
+
+export default Notice;
+
+const StyledNotice = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  width: 100%;
+  max-width: 1440px;
+  height: 100%;
+  gap: 48px;
+  margin: 0 auto;
+  position: relative;
+  padding: 82px 204px;
+`;
