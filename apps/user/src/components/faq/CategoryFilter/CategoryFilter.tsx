aad96c874128d3c0c903f4af66a82affc0d47d29
junghongseop <a01082372487@gmail.com>
2025-02-13 18:11:40 +0900

    feat(user): 카테고리 필터 컴포넌트 개발

---
+++ apps/user/src/components/faq/CategoryFilter/CategoryFilter.tsx
@@ -0,0 +1,46 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+import styled from 'styled-components';
+import Category from './Category/Category';
+import { flex } from '@maru/utils';
+
+interface CategoryFilterProps {
+  setCategory: Dispatch<SetStateAction<string>>;
+}
+
+const CATEGORY_LIST = [
+  { id: 0, category: '질문 TOP', value: 'TOP_QUESTION' },
+  { id: 1, category: '입학 과정', value: 'ADMISSION_PROCESS' },
+  { id: 2, category: '학교 생활', value: 'SCHOOL_LIFE' },
+  { id: 3, category: '관련 서류 제출', value: 'SUBMIT_DOCUMENT' },
+];
+
+const CategoryFilter = ({ setCategory }: CategoryFilterProps) => {
+  const [selectedCategory, setSelectedCategory] = useState(0);
+
+  return (
+    <StyledCategoryFilter>
+      {CATEGORY_LIST.map(({ id, category, value }) => (
+        <Category
+          key={id}
+          isSelected={selectedCategory === id}
+          onClick={() => {
+            setCategory(value);
+            setSelectedCategory(id);
+          }}
+        >
+          {category}
+        </Category>
+      ))}
+    </StyledCategoryFilter>
+  );
+};
+
+export default CategoryFilter;
+
+const StyledCategoryFilter = styled.div`
+  ${flex({ alignItems: 'center' })}
+  width: 100%;
+  height: 36px;
+  gap: 12px;
+  margin: 36px 0 40px;
+`;
---
+++ apps/user/src/components/faq/index.ts
@@ -0,0 +1,2 @@
+export { default as CategoryFilter } from './CategoryFilter/CategoryFilter';
+export { default as FaqList } from './FaqList/FaqList';
