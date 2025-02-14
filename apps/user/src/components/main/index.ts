cca524ef66a73b659db3d7f63dc16b9449795197
junghongseop <a01082372487@gmail.com>
2025-02-15 02:00:56 +0900

    feat(user): Faq box 컴포넌트 개발

--- apps/user/src/components/main/index.ts
+++ apps/user/src/components/main/index.ts
@@ -3,3 +3,4 @@ export { default as GuidelineBox } from './GuidelineBox/GuidelineBox';
 export { default as ApplicationBox } from './ApplicationBox/ApplicationBox';
 export { default as SimulatorBox } from './SimulatorBox/SimulatorBox';
 export { default as AdmissionTimelineBox } from './AdmissionTimelineBox/AdmissionTimelineBox';
+export { default as FaqBox } from './FaqBox/FaqBox';
