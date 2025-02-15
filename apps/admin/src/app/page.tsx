923c104f991639a5daeebbea82e694e250b49889
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-02-15 16:21:38 +0900

    feat(admin): 로그인 페이지 디자인에 따른 퍼블리싱

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import { color } from '@maru/design-system';
-import { Button, Column, Input } from '@maru/ui';
+import { Button, Column, Input, PreviewInput } from '@maru/ui';
 import { flex } from '@maru/utils';
 import Image from 'next/image';
 import { styled } from 'styled-components';
@@ -10,12 +10,12 @@ const LoginPage = () => {
   return (
     <StyledLogin>
       <LoginBox>
-        <Column gap={56} alignItems="center">
+        <Column gap={56} alignItems="center" width={446}>
           <Image src="/svg/maruLogo.svg" width={232} height={70} alt="logo" />
-          <Column gap={36}>
+          <Column gap={36} width="100%">
             <Column gap={24}>
               <Input label="전화번호" width="100%" name="phoneNumber" />
-              <Input label="비밀번호" width="100%" name="password" />
+              <PreviewInput label="비밀번호" width="100%" name="password" />
             </Column>
             <Column gap={16}>
               <Button width="100%">로그인</Button>
@@ -33,12 +33,12 @@ const StyledLogin = styled.div`
   ${flex({ justifyContent: 'center' })}
   width: 100%;
   height: 100vh;
+  background-color: ${color.gray100};
 `;
 
 const LoginBox = styled.div`
-  ${flex({ flexDirection: 'column', alignItems: 'center' })}
-  justify-content: center;
-  width: calc(100% - 311px);
+  ${flex({ flexDirection: 'column', alignItems: 'center', justifyContent: 'center' })}
+  width: 818px;
   height: 100%;
   background-color: ${color.white};
 `;
