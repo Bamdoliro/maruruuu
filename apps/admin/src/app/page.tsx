30aadff94b46a5da6354e780f5aa8d6cc3871dcd
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-02-15 13:47:15 +0900

    feat(admin): 로그인 페이지 퍼블리싱

--- apps/admin/src/app/page.tsx
+++ apps/admin/src/app/page.tsx
@@ -1,9 +1,44 @@
-import React from "react";
+'use client';
 
-export default function Home() {
+import { color } from '@maru/design-system';
+import { Button, Column, Input } from '@maru/ui';
+import { flex } from '@maru/utils';
+import Image from 'next/image';
+import { styled } from 'styled-components';
+
+const LoginPage = () => {
   return (
-    <div>
-      <h1>마루 어드민의 home 임</h1>
-    </div>
+    <StyledLogin>
+      <LoginBox>
+        <Column gap={56} alignItems="center">
+          <Image src="/svg/maruLogo.svg" width={232} height={70} alt="logo" />
+          <Column gap={36}>
+            <Column gap={24}>
+              <Input label="전화번호" width="100%" name="phoneNumber" />
+              <Input label="비밀번호" width="100%" name="password" />
+            </Column>
+            <Column gap={16}>
+              <Button width="100%">로그인</Button>
+            </Column>
+          </Column>
+        </Column>
+      </LoginBox>
+    </StyledLogin>
   );
-}
+};
+
+export default LoginPage;
+
+const StyledLogin = styled.div`
+  ${flex({ justifyContent: 'center' })}
+  width: 100%;
+  height: 100vh;
+`;
+
+const LoginBox = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'center' })}
+  justify-content: center;
+  width: calc(100% - 311px);
+  height: 100%;
+  background-color: ${color.white};
+`;
