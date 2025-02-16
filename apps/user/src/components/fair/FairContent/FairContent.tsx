242d2f1fc5a4f91106c42d7f1bde66cc5e474a71
junghongseop <a01082372487@gmail.com>
2025-02-16 22:04:59 +0900

    feat(user): 조회한 입학 설명회 Box 렌더링하는 Content 컴포넌트 개발

---
+++ apps/user/src/components/fair/FairContent/FairContent.tsx
@@ -0,0 +1,70 @@
+import { color } from '@maru/design-system';
+import { Column, Text } from '@maru/ui';
+import FairList from '../FairList/FairList';
+import { styled } from 'styled-components';
+import { flex } from '@maru/utils';
+
+interface FairContentProps {
+  category: string;
+}
+
+const FairContent = ({ category }: FairContentProps) => {
+  return (
+    <>
+      {category === '진행 중인 신청' && (
+        <>
+          <Column gap={16}>
+            <Text fontType="H3" color={color.gray900}>
+              학생
+            </Text>
+            <FairList
+              fairType="STUDENT_AND_PARENT"
+              status={['APPLICATION_IN_PROGRESS']}
+            />
+          </Column>
+          <Separator />
+          <Column gap={16}>
+            <Text fontType="H3" color={color.gray900}>
+              교사
+            </Text>
+            <FairList fairType="TEACHER" status={['APPLICATION_IN_PROGRESS']} />
+          </Column>
+        </>
+      )}
+      {category === '마감된 신청' && (
+        <>
+          <Column gap={16}>
+            <Text fontType="H3" color={color.gray900}>
+              학생
+            </Text>
+            <FairList
+              fairType="STUDENT_AND_PARENT"
+              status={['APPLICATION_CLOSED', 'APPLICATION_EARLY_CLOSED']}
+            />
+          </Column>
+          <Separator />
+          <Column gap={16}>
+            <Text fontType="H3" color={color.gray900}>
+              교사
+            </Text>
+            <FairList
+              fairType="TEACHER"
+              status={['APPLICATION_CLOSED', 'APPLICATION_EARLY_CLOSED']}
+            />
+          </Column>
+        </>
+      )}
+    </>
+  );
+};
+
+export default FairContent;
+
+const Separator = styled.p`
+  border: 1px solid ${color.gray400};
+  margin: 2% 0;
+  width: 100%;
+  max-width: 830px;
+  margin-top: 56px;
+  margin-bottom: 56px;
+`;
---
+++ apps/user/src/components/fair/index.ts
@@ -0,0 +1,2 @@
+export { default as FairCategory } from './FairCategory/FairCategory';
+export { default as FairContent } from './FairContent/FairContent';
