02cb0fee3a13dc90e6b5df0464afb51aaf88b145
junghongseop <a01082372487@gmail.com>
2025-02-16 22:03:28 +0900

    feat(user): FairList 컴포넌트 개발

---
+++ apps/user/src/components/fair/FairList/FairList.tsx
@@ -0,0 +1,57 @@
+import { useFairListQuery } from '@/services/fair/queries';
+import styled from 'styled-components';
+import FairItem from './FairItem/FairItem';
+import { Text } from '@maru/ui';
+import { color } from '@maru/design-system';
+import { flex } from '@maru/utils';
+
+interface FairListProps {
+  fairType: string;
+  status: string[];
+}
+
+const FairList = ({ fairType, status }: FairListProps) => {
+  const { data: fairListData } = useFairListQuery(fairType);
+
+  if (!fairListData || fairListData.length === 0) {
+    return (
+      <StyledFairListEmpty>
+        <Text fontType="H5" color={color.gray500}>
+          현재 등록된 입학 설명회는 없습니다.
+        </Text>
+      </StyledFairListEmpty>
+    );
+  }
+
+  return (
+    <StyledFairList>
+      {fairListData
+        .filter(({ status: itemStatus }) => status.includes(itemStatus))
+        .map(
+          ({ start, place, status, applicationStartDate, applicationEndDate }, index) => (
+            <FairItem
+              key={`fair ${index}`}
+              place={place}
+              start={start}
+              status={status}
+              applicationEndDate={applicationEndDate}
+              applicationStartDate={applicationStartDate}
+            />
+          )
+        )}
+    </StyledFairList>
+  );
+};
+
+export default FairList;
+
+const StyledFairList = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(400px, 1fr));
+`;
+
+const StyledFairListEmpty = styled.div`
+  ${flex({ alignItems: 'center', justifyContent: 'center' })}
+  width: 100%;
+  max-width: 816px;
+`;
