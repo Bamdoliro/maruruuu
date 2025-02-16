c6a68d66a1104c6ff7c627b8be6d38b5ae57a5d6
junghongseop <a01082372487@gmail.com>
2025-02-16 18:17:14 +0900

    feat(user): 입학 설명회 조회, 신청 api연동

---
+++ apps/user/src/services/fair/api.ts
@@ -0,0 +1,18 @@
+import { maru } from '@/apis/instance/instance';
+import { FairApplication } from '@/types/fair/client';
+import { GetFairListRes } from '@/types/fair/remote';
+
+export const getFairList = async (fairType: string) => {
+  const { data } = await maru.get<GetFairListRes>(`/fair?type=${fairType}`);
+
+  return data;
+};
+
+export const postFairApplication = async (
+  fairId: number,
+  fairApplicationData: FairApplication
+) => {
+  const { data } = await maru.post(`/fair/${fairId}`, fairApplicationData);
+
+  return data;
+};
