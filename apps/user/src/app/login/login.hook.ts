6cae70450a2009a312915c7117f0e00ffc55f59c
junghongseop <a01082372487@gmail.com>
2025-02-19 02:35:36 +0900

    feat(user): 로그인 페이지 개발

---
+++ apps/user/src/app/login/login.hook.ts
@@ -0,0 +1,55 @@
+import { ROUTES } from '@/constants/common/constants';
+import { useLoginMutation } from '@/services/auth/mutations';
+import { PostLoginReq } from '@/types/auth/remote';
+import { useRouter } from 'next/navigation';
+import { ChangeEventHandler, useEffect, useState } from 'react';
+
+export const useLoginAction = (loginData: PostLoginReq) => {
+  const { loginMutate } = useLoginMutation(loginData);
+
+  const handleLogin = () => {
+    loginMutate();
+  };
+
+  return { handleLogin };
+};
+
+export const useInput = () => {
+  const [login, setLogin] = useState<PostLoginReq>({
+    phoneNumber: '',
+    password: '',
+  });
+
+  const handleLoginChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+    setLogin({ ...login, [name]: value });
+  };
+
+  return { login, handleLoginChange };
+};
+
+export const useCTAButton = () => {
+  const router = useRouter();
+
+  const handleMoveMainPage = () => {
+    router.push(ROUTES.MAIN);
+  };
+
+  return { handleMoveMainPage };
+};
+
+export const useKeyDown = (login: () => void) => {
+  useEffect(() => {
+    const handleEnterKeyPress = (e: KeyboardEvent) => {
+      if (e.key === 'Enter') {
+        login();
+      }
+    };
+
+    document.addEventListener('keydown', handleEnterKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleEnterKeyPress);
+    };
+  }, [login]);
+};
