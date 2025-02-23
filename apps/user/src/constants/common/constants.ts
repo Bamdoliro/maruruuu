fa7aadba1d6cf4f46db38e9cc54e24b0ad50ddf3
junghongseop <a01082372487@gmail.com>
2025-02-23 17:05:22 +0900

    chore(user): 입학 등록 url 변경

--- apps/user/src/constants/common/constants.ts
+++ apps/user/src/constants/common/constants.ts
@@ -4,7 +4,7 @@ export const ROUTES = {
   FORM_MANAGEMENT: '/management',
   NOTICE: '/notice',
   FAQ: '/faq',
-  ADMISSION_REGISTRATION: '/enroll',
+  ADMISSION_REGISTRATION: '/enrollment',
   TERMS_OF_SERVICE: '/tos',
   PRIVACY_POLCY: '/privacy',
   PERSONAL_INFO_COLLECTION: '/collection',
