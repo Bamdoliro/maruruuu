a8c45248f179b9d975c4a22476130058fd945088
junghongseop <a01082372487@gmail.com>
2025-02-24 22:42:35 +0900

    feat(user): 파일이 업로드 되는 동안 보여질 로더 컴포넌트 개발

--- apps/user/src/components/enrollment/index.ts
+++ apps/user/src/components/enrollment/index.ts
@@ -1,2 +1,3 @@
 export { default as FileUploader } from './FileUploader/FileUploader';
 export { default as ExplainBox } from './ExplainBox/ExplainBox';
+export { default as EntrollmentLoader } from './EntrollmentLoader/EntrollmentLoader';
