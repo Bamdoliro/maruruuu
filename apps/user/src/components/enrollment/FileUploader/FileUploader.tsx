1cd17aad4bc0a056edba7affb3bfa41019c804f8
junghongseop <a01082372487@gmail.com>
2025-02-24 21:55:01 +0900

    fix(user): 파일 업로더 ref 위치 수정

--- apps/user/src/components/enrollment/FileUploader/FileUploader.tsx
+++ apps/user/src/components/enrollment/FileUploader/FileUploader.tsx
@@ -1,29 +1,30 @@
 import { color } from '@maru/design-system';
 import { Button, Row, Text } from '@maru/ui';
-import { ChangeEventHandler, ForwardedRef } from 'react';
+import { ChangeEventHandler, ForwardedRef, forwardRef } from 'react';
 
 interface FileUploaderProps {
   onClick: () => void;
   onChange: ChangeEventHandler<HTMLInputElement>;
   document: string;
-  ref: ForwardedRef<HTMLInputElement>;
 }
 
-const FileUploader = ({ onClick, onChange, document, ref }: FileUploaderProps) => {
-  return (
-    <>
-      <Row gap={16} alignItems="center">
-        <Button size="SMALL" onClick={onClick}>
-          첨부파일 업로드
-        </Button>
-        <Text fontType="p2" color={color.gray900}>
-          {document || '선택된 파일 없음'}
-        </Text>
-      </Row>
-      <input ref={ref} onChange={onChange} type="file" accept=".pdf, .hwpx" hidden />
-    </>
-  );
-};
+const FileUploader = forwardRef<HTMLInputElement, FileUploaderProps>(
+  ({ onClick, document, onChange }, ref) => {
+    return (
+      <>
+        <Row gap={16} alignItems="center">
+          <Button size="SMALL" onClick={onClick}>
+            첨부파일 업로드
+          </Button>
+          <Text fontType="p2" color={color.gray900}>
+            {document || '선택된 파일 없음'}
+          </Text>
+        </Row>
+        <input ref={ref} onChange={onChange} type="file" accept=".pdf, .hwpx" hidden />
+      </>
+    );
+  }
+);
 
 FileUploader.displayName = 'FileUploader';
 
