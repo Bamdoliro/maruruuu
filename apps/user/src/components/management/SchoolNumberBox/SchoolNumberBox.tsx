30924dcc7a0751ac7772a1d44a451563aea574bf
junghongseop <a01082372487@gmail.com>
2025-02-27 02:37:46 +0900

    feat(user): 학교 전화번호 박스 컴포넌트 개발

---
+++ apps/user/src/components/management/SchoolNumberBox/SchoolNumberBox.tsx
@@ -0,0 +1,53 @@
+import { color } from '@maru/design-system';
+import { IconPhone } from '@maru/icon';
+import { Column, Row, Text } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { styled } from 'styled-components';
+
+interface SchoolNumberBoxProps {
+  size: 'big' | 'small';
+}
+
+const SchoolNumberBox = ({ size }: SchoolNumberBoxProps) => {
+  return (
+    <StyledSchoolNumberBox size={size}>
+      <Row gap={12} alignItems="center">
+        <IconPhone width={36} height={36} fill={color.maruDefault} />
+        <Text fontType={size === 'big' ? 'H2' : 'H3'} color={color.gray900}>
+          입학 문의 전화
+        </Text>
+        {size === 'small' && (
+          <Text fontType="H4" color={color.gray600}>
+            교무실
+          </Text>
+        )}
+      </Row>
+      <Column alignItems="flex-start" gap={4}>
+        {size === 'big' && (
+          <Text fontType="H4" color={color.gray600}>
+            교무실
+          </Text>
+        )}
+        <Text fontType={size === 'big' ? 'H1' : 'H4'} color={color.gray900}>
+          051-971-2153
+        </Text>
+      </Column>
+    </StyledSchoolNumberBox>
+  );
+};
+
+export default SchoolNumberBox;
+
+const StyledSchoolNumberBox = styled.div<{ size: 'big' | 'small' }>`
+  ${flex({
+    flexDirection: 'column',
+    alignItems: 'flex-start',
+    justifyContent: 'space-between',
+  })}
+  width: 100%;
+  height: ${({ size }) => (size === 'big' ? '415px' : '141px')};
+  padding: ${({ size }) => (size === 'big' ? '48px 36px' : '24px 32px')};
+  border-radius: 12px;
+  border: 1px solid ${color.gray200};
+  background-color: ${color.white};
+`;
