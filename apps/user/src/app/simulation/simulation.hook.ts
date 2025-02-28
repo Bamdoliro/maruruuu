e804281f1e2a19cc37dcdf3ed1afe253ccbed796
junghongseop <a01082372487@gmail.com>
2025-03-01 01:37:24 +0900

    feat(user): 성적 모의 계산 hook 개발

---
+++ apps/user/src/app/simulation/simulation.hook.ts
@@ -0,0 +1,20 @@
+import { useFormValueStore } from '@/stores';
+import { useState } from 'react';
+
+export const useNavigationClick = () => {
+  const form = useFormValueStore();
+  const [currentStep, setCurrentStep] = useState('성적 입력');
+
+  const handleMoveStep = (step: string) => {
+    const qualificationExam =
+      form.education.graduationType === 'QUALIFICATION_EXAMINATION';
+    const nowStep = step === '출결상황' || step === '봉사시간';
+
+    if (qualificationExam && nowStep) {
+      alert('검정고시 합격자는 기본 점수가 부여돼요.');
+    }
+    setCurrentStep(step);
+  };
+
+  return { handleMoveStep, currentStep };
+};
