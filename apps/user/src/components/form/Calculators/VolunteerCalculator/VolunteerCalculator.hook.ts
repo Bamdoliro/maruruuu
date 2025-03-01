f70247557a02068b66b6eabcad748e49c0cde863
junghongseop <a01082372487@gmail.com>
2025-03-01 22:14:47 +0900

    feat(user): 봉사 시간 컴포넌트 개발

---
+++ apps/user/src/components/form/Calculators/VolunteerCalculator/VolunteerCalculator.hook.ts
@@ -0,0 +1,12 @@
+import { useSetFormStore } from '@/stores';
+import type { ChangeEventHandler } from 'react';
+
+export const useInput = () => {
+  const setForm = useSetFormStore();
+
+  const handleVolunteerTimeChange: ChangeEventHandler<HTMLInputElement> = (e) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, grade: { ...prev.grade, [name]: Number(value) } }));
+  };
+  return { handleVolunteerTimeChange };
+};
---
+++ apps/user/src/components/form/Calculators/VolunteerCalculator/VolunteerCalculator.tsx
@@ -0,0 +1,90 @@
+import { useFormValueStore } from '@/stores';
+import { color, font } from '@maru/design-system';
+import { CellInput, Column, Row, Td, Text, Th } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { styled } from 'styled-components';
+import { useInput } from './VolunteerCalculator.hook';
+
+const VolunteerCalculator = () => {
+  const form = useFormValueStore();
+  const { handleVolunteerTimeChange } = useInput();
+
+  const isReadOnly = form.education.graduationType === 'QUALIFICATION_EXAMINATION';
+
+  return (
+    <StyledVolunteerCalculator>
+      <Text fontType="p3" color={color.red}>
+        *2024.09.30까지의 봉사시간을 기재해주세요. 졸업생은 졸업일 기준으로 기재해주세요.
+      </Text>
+      <Column>
+        <Row>
+          <Th borderTopLeftRadius={12} width="20%" height={56}>
+            학년
+          </Th>
+          <Th borderTopRightRadius={12} width="80%" height={56}>
+            봉사시간
+          </Th>
+        </Row>
+        <Row>
+          <Td width="20%" height={56} styleType="SECONDARY">
+            1학년
+          </Td>
+          <Td width="80%" height={56}>
+            <CellInput
+              name="volunteerTime1"
+              onChange={handleVolunteerTimeChange}
+              value={form.grade.volunteerTime1}
+              isError={Number(form.grade.volunteerTime1) < 0}
+              readOnly={isReadOnly}
+            />
+            <Hour>시간</Hour>
+          </Td>
+        </Row>
+        <Row>
+          <Td width="20%" height={56} styleType="SECONDARY">
+            2학년
+          </Td>
+          <Td width="80%" height={56}>
+            <CellInput
+              name="volunteerTime2"
+              onChange={handleVolunteerTimeChange}
+              value={form.grade.volunteerTime2}
+              isError={Number(form.grade.volunteerTime2) < 0}
+              readOnly={isReadOnly}
+            />
+            <Hour>시간</Hour>
+          </Td>
+        </Row>
+        <Row>
+          <Td borderBottomLeftRadius={12} width="20%" height={56} styleType="SECONDARY">
+            3학년
+          </Td>
+          <Td borderBottomRightRadius={12} width="80%" height={56}>
+            <CellInput
+              name="volunteerTime3"
+              onChange={handleVolunteerTimeChange}
+              value={form.grade.volunteerTime3}
+              isError={Number(form.grade.volunteerTime3) < 0}
+              readOnly={isReadOnly}
+            />
+            <Hour>시간</Hour>
+          </Td>
+        </Row>
+      </Column>
+    </StyledVolunteerCalculator>
+  );
+};
+
+export default VolunteerCalculator;
+
+const StyledVolunteerCalculator = styled.div`
+  ${flex({ flexDirection: 'column' })};
+  width: 100%;
+  gap: 16px;
+`;
+
+const Hour = styled.p`
+  ${font.p2}
+  color: ${color.gray900};
+  margin-left: 8px;
+`;
