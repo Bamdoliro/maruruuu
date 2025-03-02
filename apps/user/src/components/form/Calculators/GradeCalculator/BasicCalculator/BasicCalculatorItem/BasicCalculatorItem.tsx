987704ce6d679318fc6a43fa6d9fecb0afc22a23
junghongseop <a01082372487@gmail.com>
2025-03-02 15:53:33 +0900

    fix(user): 일반 성적 계산기 렌더링 수정

--- apps/user/src/components/form/Calculators/GradeCalculator/BasicCalculator/BasicCalculatorItem/BasicCalculatorItem.tsx
+++ apps/user/src/components/form/Calculators/GradeCalculator/BasicCalculator/BasicCalculatorItem/BasicCalculatorItem.tsx
@@ -3,12 +3,19 @@ import { flex } from '@maru/utils';
 import styled from 'styled-components';
 import { useSubjectIncompleteValueStore, useSubjectListValueStore } from '@/stores';
 import { useInput } from './BasicCalculatorItem.hook';
+import { Subject } from '@/types/form/client';
 
 interface Props {
   id: number;
   achievementLevels: string[];
   isError?: boolean[];
 }
+
+const ACHIEVEMENT_KEYS: (keyof Subject)[] = [
+  'achievementLevel21',
+  'achievementLevel22',
+  'achievementLevel31',
+];
 
 const BasicCalculatorItem = ({ id, achievementLevels, isError = [] }: Props) => {
   const subjectList = useSubjectListValueStore();
@@ -17,9 +24,9 @@ const BasicCalculatorItem = ({ id, achievementLevels, isError = [] }: Props) => 
 
   const subject = subjectList[id];
 
-  const getDisplayValue = (value: string | null, incomplete: boolean | null) => {
+  const getDisplayValue = (value: string | number | null, incomplete: boolean | null) => {
     if (value === null || (incomplete && value === 'C')) return '미이수';
-    return value;
+    return value as string;
   };
 
   return (
@@ -27,48 +34,22 @@ const BasicCalculatorItem = ({ id, achievementLevels, isError = [] }: Props) => 
       <Td styleType="SECONDARY" width="15.3%" height={64}>
         {subject.subjectName}
       </Td>
-      <Td width="24%" height={64}>
-        <Dropdown
-          value={getDisplayValue(
-            subject.achievementLevel21,
-            subjectIncomplete[subject.subjectName]?.isIncomplete21
-          )}
-          size="SMALL"
-          data={achievementLevels}
-          width={80}
-          onChange={handleSubjectChange}
-          name="achievementLevel21"
-          isError={subject.achievementLevel21 === '-' && isError[id]}
-        />
-      </Td>
-      <Td width="24%" height={64}>
-        <Dropdown
-          value={getDisplayValue(
-            subject.achievementLevel22,
-            subjectIncomplete[subject.subjectName]?.isIncomplete22
-          )}
-          size="SMALL"
-          data={achievementLevels}
-          width={80}
-          onChange={handleSubjectChange}
-          name="achievementLevel22"
-          isError={subject.achievementLevel22 === '-' && isError[id]}
-        />
-      </Td>
-      <Td width="24%" height={64}>
-        <Dropdown
-          value={getDisplayValue(
-            subject.achievementLevel31,
-            subjectIncomplete[subject.subjectName]?.isIncomplete31
-          )}
-          size="SMALL"
-          data={achievementLevels}
-          width={80}
-          onChange={handleSubjectChange}
-          name="achievementLevel31"
-          isError={subject.achievementLevel31 === '-' && isError[id]}
-        />
-      </Td>
+      {ACHIEVEMENT_KEYS.map((key) => (
+        <Td key={key} width="24%" height={64}>
+          <Dropdown
+            value={getDisplayValue(
+              subject[key],
+              subjectIncomplete[subject.subjectName]?.isIncomplete21
+            )}
+            size="SMALL"
+            data={achievementLevels}
+            width={80}
+            onChange={handleSubjectChange}
+            name={key}
+            isError={subject[key] === '-' && isError[id]}
+          />
+        </Td>
+      ))}
       <Td width="15%" height={64}>
         {null}
       </Td>
