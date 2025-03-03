2c2988a1236a0ad3d47a2bd81b2d2d0a2671b057
junghongseop <a01082372487@gmail.com>
2025-03-03 23:53:40 +0900

    feat(user): 원서 상태에 따른 페이지 이동 컴포넌트 개발

---
+++ apps/user/src/components/common/FormWrapper/FormStatusManager/FormStatusManager.tsx
@@ -0,0 +1,26 @@
+import { useFormStatusQuery } from '@/services/form/queries';
+import { useSetFormStepStore } from '@/stores';
+import { useEffect } from 'react';
+
+const FormStatusManager = () => {
+  const { data: formStatusData } = useFormStatusQuery();
+  const setFormStep = useSetFormStepStore();
+
+  useEffect(() => {
+    if (formStatusData) {
+      const { status } = formStatusData;
+
+      if (status === 'SUBMITTED') setFormStep('초안제출완료');
+      else if (
+        status === 'RECEIVED' ||
+        status === 'APPROVED' ||
+        status === 'FINAL_SUBMITTED'
+      )
+        setFormStep('최종제출완료');
+    }
+  }, [formStatusData, setFormStep]);
+
+  return null;
+};
+
+export default FormStatusManager;
