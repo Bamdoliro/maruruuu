f3c350553bc2cda659182f1c694cb7686d69b6d1
junghongseop <a01082372487@gmail.com>
2025-03-03 23:52:37 +0900

    feat(user): 저장된 원서 가져오는 매니저 컴포넌트 개발

---
+++ apps/user/src/components/common/FormWrapper/SaveFormManager/SaveFormManager.tsx
@@ -0,0 +1,52 @@
+import { useSaveFormQuery } from '@/services/form/queries';
+import {
+  useIsSaveFormLoadedStore,
+  useSetFormStore,
+  useSetGEDSubjectListStore,
+  useSetNewGEDSubjectListStore,
+  useSetNewSubjectListStore,
+  useSetSubjectListStore,
+} from '@/stores';
+import { Subject } from '@/types/form/client';
+import { updateSlicedSubjectList } from '@/utils';
+import { useEffect } from 'react';
+import { SetterOrUpdater } from 'recoil';
+
+const SaveFormManager = () => {
+  const { data: saveFormData } = useSaveFormQuery();
+  const [isSaveFormLoaded, setIsSaveFormLoaded] = useIsSaveFormLoadedStore();
+  const setForm = useSetFormStore();
+  const setSubjectList = useSetSubjectListStore();
+  const setNewSubjectList = useSetNewSubjectListStore();
+  const setGEDSubjectList = useSetGEDSubjectListStore();
+  const setNewGEDSubjectList = useSetNewGEDSubjectListStore();
+
+  useEffect(() => {
+    if (!saveFormData || isSaveFormLoaded) return;
+
+    const subjectList = saveFormData.grade.subjectList;
+    const graduationType = saveFormData.education.graduationType;
+
+    setForm((prev) => ({ ...prev, ...saveFormData }));
+
+    if (subjectList) {
+      const updateSubjects: [
+        SetterOrUpdater<Subject[]>,
+        SetterOrUpdater<Subject[]>,
+        number,
+      ] =
+        graduationType === 'QUALIFICATION_EXAMINATION'
+          ? [setGEDSubjectList, setNewGEDSubjectList, 5]
+          : [setSubjectList, setNewSubjectList, 12];
+
+      updateSubjects[0](updateSlicedSubjectList(subjectList, 0, updateSubjects[2]));
+      updateSubjects[1](updateSlicedSubjectList(subjectList, updateSubjects[2]));
+    }
+
+    setIsSaveFormLoaded(true);
+  }, [isSaveFormLoaded, saveFormData]);
+
+  return null;
+};
+
+export default SaveFormManager;
