a026a214acafa226a018135155f524e91bca0d58
junghongseop <a01082372487@gmail.com>
2025-03-03 23:43:56 +0900

    feat(user): 저장된 원서의 과목을 가져올때 사용할 유틸리티 개발

--- apps/user/src/utils/index.ts
+++ apps/user/src/utils/index.ts
@@ -4,3 +4,4 @@ export { default as formatStartDate } from './formatStartDate';
 export { default as formatStatus } from './formatStatus';
 export { default as formatApplicationDate } from './formatApplicationDate';
 export { default as getAchivementLevel } from './getAchivementLevel';
+export { default as updateSlicedSubjectList } from './updateSlicedSubjectList';
