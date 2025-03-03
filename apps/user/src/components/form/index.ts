d95a79c98a83afe19d25a01f7be13f38475ebf1b
junghongseop <a01082372487@gmail.com>
2025-03-04 08:59:28 +0900

    feat(user): 원서 작성 단계를 보여주는 컴포넌트 개발

--- apps/user/src/components/form/index.ts
+++ apps/user/src/components/form/index.ts
@@ -3,3 +3,4 @@ export { default as GradeCalculator } from './Calculators/GradeCalculator/GradeC
 export { default as AttendanceCalculator } from './Calculators/AttendanceCalculator/AttendanceCalculator';
 export { default as VolunteerCalculator } from './Calculators/VolunteerCalculator/VolunteerCalculator';
 export { default as CertificateCalculator } from './Calculators/CertificateCalculator/CertificateCalculator';
+export { default as ProgressStage } from './ProgressStage/ProgressStage';
