3812ed51fb168d3d5a97df202cc0377509d73b78
junghongseop <a01082372487@gmail.com>
2025-03-04 09:00:24 +0900

    feat(user): 공통 원서 레이아웃 개발

---
+++ apps/user/src/layouts/FormLayout.tsx
@@ -0,0 +1,48 @@
+import { Header } from '@/components/common';
+import { ProgressStage } from '@/components/form';
+import { color } from '@maru/design-system';
+import { Text } from '@maru/ui';
+import { ReactNode } from 'react';
+import styled from 'styled-components';
+
+interface FormLayoutProps {
+  children: ReactNode;
+  title: string;
+}
+
+const FormLayout = ({ children, title }: FormLayoutProps) => {
+  return (
+    <>
+      <Header />
+      <StyledFormLayout>
+        <ProgressStage />
+        <FormBox>
+          <Text fontType="H3" color={color.gray900}>
+            {title}
+          </Text>
+          <FormContentBox>{children}</FormContentBox>
+        </FormBox>
+      </StyledFormLayout>
+    </>
+  );
+};
+
+export default FormLayout;
+
+const StyledFormLayout = styled.div`
+  width: 100%;
+  margin-bottom: 140px;
+  background-color: ${color.white};
+`;
+
+const FormBox = styled.div`
+  width: 100%;
+  margin: 0 auto;
+  padding: 0px 312px 61px;
+`;
+
+const FormContentBox = styled.div`
+  width: 100%;
+  height: 100%;
+  margin-top: 40px;
+`;
