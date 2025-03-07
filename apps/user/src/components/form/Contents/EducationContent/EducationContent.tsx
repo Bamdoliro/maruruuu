9e73961d81d7dd3d9a1a0fa60a835875f1ad097e
junghongseop <a01082372487@gmail.com>
2025-03-07 10:26:23 +0900

    feat(user): 출신학교및 학력 컨텐트 컴포넌트 개발

---
+++ apps/user/src/components/form/Contents/EducationContent/EducationContent.tsx
@@ -0,0 +1,150 @@
+import { styled } from 'styled-components';
+import FormController from '../../FormController/FormController';
+import { ButtonInput, Column, Input, RadioGroup, Row } from '@maru/ui';
+import { useFormValueStore } from '@/stores';
+import { useCTAButton, useInput } from './EducationContent.hook';
+import { useOverlay } from '@toss/use-overlay';
+import FindSchoolModal from '../../FindSchoolModal/FindSchoolModal';
+
+const EducationContent = () => {
+  const overlay = useOverlay();
+  const form = useFormValueStore();
+  const { handleNextStep, handlePreviousStep } = useCTAButton();
+  const { handleEducationChange } = useInput();
+
+  const openFindSchoolModal = () => {
+    overlay.open(({ isOpen, close }) => (
+      <FindSchoolModal isOpen={isOpen} onClose={close} />
+    ));
+  };
+
+  return (
+    <>
+      <Column width="100%" gap={30}>
+        <RadioGroup
+          label="졸업 구분"
+          name="graduationType"
+          items={[
+            { value: 'EXPECTED', label: '졸업 예정' },
+            { value: 'GRADUATED', label: '졸업' },
+            { value: 'QUALIFICATION_EXAMINATION', label: '고입 검정' },
+          ]}
+          value={form.education.graduationType}
+          onChange={handleEducationChange}
+        />
+        <Row gap={48} alignItems="center">
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <ButtonInput
+              name="schoolName"
+              label="출신학교"
+              value={form.education.schoolName ?? ''}
+              buttonText="검색"
+              onClick={openFindSchoolModal}
+              placeholder="검색 버튼을 눌러 학교를 검색하세요."
+              readOnly
+              enabled={true}
+            />
+          )}
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <Input
+              name="schoolAddress"
+              label="도로명주소"
+              placeholder="학교를 선택하면 자동완성됩니다."
+              width="100%"
+              readOnly
+              value={form.education.schoolAddress ?? ''}
+              onChange={handleEducationChange}
+            />
+          )}
+        </Row>
+        <Row gap={48} alignItems="center">
+          <Input
+            name="graduationYear"
+            label={
+              form.education.graduationType === 'QUALIFICATION_EXAMINATION'
+                ? '합격연도'
+                : '졸업(예정)연도'
+            }
+            placeholder="예) 2024"
+            width={
+              form.education.graduationType === 'QUALIFICATION_EXAMINATION'
+                ? '50%'
+                : '100%'
+            }
+            onChange={handleEducationChange}
+          />
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <Input
+              name="schoolLocation"
+              label="지역"
+              placeholder="학교를 선택하면 자동완성됩니다."
+              readOnly
+              width="100%"
+              value={form.education.schoolLocation ?? ''}
+              onChange={handleEducationChange}
+            />
+          )}
+        </Row>
+        <Row gap={48} alignItems="center">
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <Input
+              name="schoolCode"
+              label="표준 학교 코드"
+              placeholder="학교를 선택하면 자동완성됩니다."
+              readOnly
+              width="100%"
+              value={form.education.schoolCode ?? ''}
+              onChange={handleEducationChange}
+            />
+          )}
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <Input
+              name="teacherPhoneNumber"
+              label="학교 대표 연락처"
+              placeholder="학교의 교무실 연락처를 입력해주세요."
+              width="100%"
+              value={form.education.teacherPhoneNumber ?? ''}
+              onChange={handleEducationChange}
+            />
+          )}
+        </Row>
+        <Row gap={48} alignItems="center">
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <Input
+              name="teacherName"
+              label="작성 교사 이름"
+              placeholder="예) 홍길동"
+              width="100%"
+              value={form.education.teacherName ?? ''}
+              onChange={handleEducationChange}
+            />
+          )}
+          {form.education.graduationType !== 'QUALIFICATION_EXAMINATION' && (
+            <Input
+              name="teacherMobilePhoneNumber"
+              label="작성 교사 연락처(휴대전화)"
+              placeholder="- 없이 입력해 주세요"
+              width="100%"
+              value={form.education.teacherMobilePhoneNumber ?? ''}
+              onChange={handleEducationChange}
+            />
+          )}
+        </Row>
+      </Column>
+      <StyledFormController graduationType={form.education.graduationType}>
+        <FormController
+          onPrevious={handlePreviousStep}
+          onNext={handleNextStep}
+          step="출신학교및학력"
+        />
+      </StyledFormController>
+    </>
+  );
+};
+
+export default EducationContent;
+
+const StyledFormController = styled.div<{ graduationType: string }>`
+  margin-top: ${(props) =>
+    props.graduationType === 'QUALIFICATION_EXAMINATION' ? '290.391px' : '0px'};
+`;
