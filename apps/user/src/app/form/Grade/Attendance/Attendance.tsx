52652ea6175adfa506e0b6f343fdeb092b043678
junghongseop <a01082372487@gmail.com>
2025-03-08 01:51:59 +0900

    feat(user): 출석 페이지 개발

---
+++ apps/user/src/app/form/Grade/Attendance/Attendance.tsx
@@ -0,0 +1,30 @@
+import { AttendanceCalculator, FormController } from '@/components/form';
+import { Column, UnderlineButton } from '@maru/ui';
+import { styled } from 'styled-components';
+import { useCTAButton } from './Attendance.hook';
+
+const Attendance = () => {
+  const { handleNextStep, handlePreviousStep } = useCTAButton();
+
+  return (
+    <>
+      <Column gap={24}>
+        <NavigationBar>
+          <UnderlineButton active={true}>출결 상황</UnderlineButton>
+        </NavigationBar>
+        <AttendanceCalculator />
+      </Column>
+      <FormController
+        onNext={handleNextStep}
+        onPrevious={handlePreviousStep}
+        step="성적입력"
+      />
+    </>
+  );
+};
+
+export default Attendance;
+
+const NavigationBar = styled.div`
+  width: 100%;
+`;
