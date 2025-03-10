5cb30a91540dde0111784b1dfce68db49bd65f6c
junghongseop <a01082372487@gmail.com>
2025-03-10 11:40:51 +0900

    feat(user): 초안 제출 완료 페이지 개발

---
+++ apps/user/src/components/form/Contents/DraftSubmissionCompletedContent/DraftSubmissionCompletedContent.tsx
@@ -0,0 +1,68 @@
+import { color } from '@maru/design-system';
+import { IconCheckCircle } from '@maru/icon';
+import { Button, Column, Row, Text } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { styled } from 'styled-components';
+import { useCTAButton } from './DraftSubmissionCompletedContent.hook';
+
+const DraftSubmissionCompletedContent = () => {
+  const { handleMoveMainPage, handleMoveFinalSubmit } = useCTAButton();
+
+  return (
+    <StyledDraftSubmissionCompletedContent>
+      <Row gap={8} alignItems="center">
+        <IconCheckCircle width={64} height={64} />
+        <Text fontType="H1" color={color.gray900}>
+          입학 서류 제출하기
+        </Text>
+      </Row>
+      <Column gap={12}>
+        <Text fontType="p1" color={color.gray900}>
+          입학원서를 제출 완료하셨습니다.
+        </Text>
+        <Text fontType="H4" color={color.red}>
+          작성한 입학 원서를 출력하여 학교장 직인을 날인 후 추가 서류와 함께
+          <br />
+          인터넷 접수하여 입학원서 접수를 완료하여 주시기 바랍니다.
+          <br />
+          <br />
+          최종 접수한 모든 서류는 우편 또는 방문 접수하여 주시기 바랍니다.
+        </Text>
+      </Column>
+      <Row gap={16}>
+        <Button onClick={handleMoveMainPage} styleType="SECONDARY" size="LARGE">
+          홈으로 돌아가기
+        </Button>
+        <Button onClick={handleMoveFinalSubmit} size="LARGE">
+          최종 제출 페이지로 이동하기
+        </Button>
+      </Row>
+    </StyledDraftSubmissionCompletedContent>
+  );
+};
+
+export default DraftSubmissionCompletedContent;
+
+const StyledDraftSubmissionCompletedContent = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  gap: 48px;
+  max-width: 800px;
+  height: 100%;
+  margin: 62px auto 0;
+
+  opacity: 0;
+  animation: show 1.2s 2s cubic-bezier(0.22, 0.61, 0.36, 1) forwards;
+
+  pointer-events: none;
+
+  @keyframes show {
+    from {
+      transform: translateY(200px);
+    }
+    to {
+      transform: translateY(0);
+      opacity: 100;
+      pointer-events: auto;
+    }
+  }
+`;
