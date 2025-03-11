0f4ea544ea30fcb2eb40f2398ac162dc4b0e463f
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-11 13:25:05 +0900

    feat(admin): 공지사항 추가 mutation hook 추가

--- apps/admin/src/services/notice/mutations.ts
+++ apps/admin/src/services/notice/mutations.ts
@@ -2,8 +2,26 @@ import { useApiError } from '@/hooks';
 import { useMutation } from '@tanstack/react-query';
 import { useRouter } from 'next/navigation';
 import { toast } from 'react-toastify';
-import { deleteNotice } from './api';
+import { deleteNotice, postNotice } from './api';
 import { ROUTES } from '@/constants/common/constant';
+import { PostNoticeReq } from '@/types/notice/remote';
+
+export const usePostNoticeMutation = (params: PostNoticeReq) => {
+  const { handleError } = useApiError();
+  const router = useRouter();
+
+  const { mutate: usePostNoticeMutate, ...restMutate } = useMutation({
+    mutationFn: (noticeData: PostNoticeReq) => postNotice(noticeData),
+    onSuccess: () => {
+      toast('공지사항이 게시되었습니다.', {
+        type: 'success',
+      });
+    },
+    onError: handleError,
+  });
+
+  return { usePostNoticeMutate, ...restMutate };
+};
 
 export const useDeleteNoticeMutation = (id: number) => {
   const { handleError } = useApiError();
