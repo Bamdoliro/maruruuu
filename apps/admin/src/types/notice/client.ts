97c24cc49e383ab50fd4efc9157cc751cb258e18
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-11 11:06:32 +0900

    feat(admin): 공지사항 디테일 interface 정의

--- apps/admin/src/types/notice/client.ts
+++ apps/admin/src/types/notice/client.ts
@@ -3,3 +3,14 @@ export interface Notice {
   title: string;
   updatedAt: string;
 }
+
+export interface NoticeDetail {
+  title: string;
+  content: string;
+  fileList?: Array<{
+    downloadUrl: string;
+    fileName: string;
+  }>;
+  createdAt: string;
+  updatedAt: string;
+}
