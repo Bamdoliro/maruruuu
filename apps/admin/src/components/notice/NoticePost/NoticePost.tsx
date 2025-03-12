ed21483be693d9d3e31ab36043faf8a1639689ce
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-12 21:08:38 +0900

    feat(admin): 공지사항 추가 페이지 개발

---
+++ apps/admin/src/components/notice/NoticePost/NoticePost.tsx
@@ -0,0 +1,179 @@
+import { useNoticeFileStore } from '@/store/notice/noticeFile';
+import { color, font } from '@maru/design-system';
+import { IconClip } from '@maru/icon';
+import { Button, Column, Row, Text } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { useOverlay } from '@toss/use-overlay';
+import { ChangeEventHandler, useRef, useState } from 'react';
+import { styled } from 'styled-components';
+import NoticeUploadModal from '../NoticeUploadModal/NoticeUploadModal';
+import { useNoticePostAction } from './NoticePost.hook';
+
+const NoticePost1 = () => {
+  const overlay = useOverlay();
+  const contentTextareaRef = useRef<HTMLTextAreaElement>(null);
+  const [fileData, setFileData] = useNoticeFileStore();
+  const [noticeData, setNoticeData] = useState<{
+    title: string;
+    content: string;
+    fileNameList: string[];
+  }>({
+    title: '',
+    content: '',
+    fileNameList: [],
+  });
+
+  const { handleNoticePostButtonClick } = useNoticePostAction(noticeData);
+
+  const handleNoticeDataChange: ChangeEventHandler<
+    HTMLInputElement | HTMLTextAreaElement
+  > = (e) => {
+    const { name, value } = e.target;
+    setNoticeData({ ...noticeData, [name]: value });
+
+    if (!contentTextareaRef.current) return;
+    contentTextareaRef.current.style.height = 'auto';
+    contentTextareaRef.current.style.height =
+      contentTextareaRef.current.scrollHeight + 'px';
+  };
+
+  const handleNoticeFileModalButtonClick = () => {
+    overlay.open(({ isOpen, close }) => (
+      <NoticeUploadModal
+        isOpen={isOpen}
+        onClose={close}
+        onFileAttach={(file) => {
+          if (file) {
+            setNoticeData((prevData) => ({
+              ...prevData,
+              fileNameList: [...prevData.fileNameList, file.name],
+            }));
+          }
+        }}
+      />
+    ));
+  };
+
+  const handleDeleteNoticeFile = (fileNameToDelete: string) => {
+    setNoticeData((prevData) => ({
+      ...prevData,
+      fileNameList: prevData.fileNameList.filter((file) => file !== fileNameToDelete),
+    }));
+    setFileData(fileData?.filter((file) => file.name !== fileNameToDelete) ?? []);
+  };
+
+  return (
+    <StyledNoticePost>
+      <NoticePostHeader>
+        <TitleInput
+          name="title"
+          value={noticeData.title}
+          onChange={handleNoticeDataChange}
+          placeholder="제목을 입력해주세요"
+        ></TitleInput>
+        <Row gap="10px">
+          <Button
+            size="SMALL"
+            icon="CLIP_ICON"
+            styleType={noticeData.fileNameList.length >= 3 ? 'DISABLED' : 'SECONDARY'}
+            width={124}
+            onClick={handleNoticeFileModalButtonClick}
+            disabled={noticeData.fileNameList.length >= 3}
+          >
+            <Text fontType="btn2">파일 첨부</Text>
+          </Button>
+          <Button size="SMALL" onClick={handleNoticePostButtonClick}>
+            게시하기
+          </Button>
+        </Row>
+      </NoticePostHeader>
+      <ContentTextarea
+        ref={contentTextareaRef}
+        name="content"
+        value={noticeData.content}
+        onChange={handleNoticeDataChange}
+        placeholder="내용을 작성해주세요."
+        rows={1}
+      />
+      {noticeData.fileNameList.length > 0 && (
+        <Column gap={12}>
+          {noticeData.fileNameList.map((file, index) => (
+            <Row alignItems="center" gap={12} key={index}>
+              <StyledNoticeFile>
+                <Row alignItems="center" gap={10}>
+                  <IconClip width={19} height={12} />
+                  {file}
+                </Row>
+              </StyledNoticeFile>
+              <DeleteButton onClick={() => handleDeleteNoticeFile(file)}>
+                <Text fontType="caption" color={color.red}>
+                  [삭제]
+                </Text>
+              </DeleteButton>
+            </Row>
+          ))}
+        </Column>
+      )}
+    </StyledNoticePost>
+  );
+};
+
+export default NoticePost1;
+
+const StyledNoticePost = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  width: 100%;
+  gap: 36px;
+`;
+
+const NoticePostHeader = styled.div`
+  ${flex({ flexDirection: 'row', justifyContent: 'space-between' })}
+  width: 100%;
+  gap: 16px;
+  border-bottom: 1px solid ${color.gray300};
+  padding-bottom: 16px;
+`;
+
+const TitleInput = styled.input`
+  ${font.H1}
+  color: ${color.gray900};
+  width: 100%;
+
+  &::placeholder {
+    color: ${color.gray400};
+  }
+`;
+
+const ContentTextarea = styled.textarea`
+  ${font.p2};
+  color: ${color.gray900};
+
+  &::placeholder {
+    color: ${color.gray500};
+  }
+  resize: none;
+`;
+
+const StyledNoticeFile = styled.div`
+  ${flex({ justifyContent: 'space-between', alignItems: 'center' })};
+  gap: 12px;
+  height: 36px;
+  padding: 0 15px 0 15px;
+  border-radius: 999px;
+  background: ${color.gray200};
+  width: auto;
+  min-width: fit-content;
+  max-width: fit-content;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${color.gray300};
+  }
+`;
+
+const DeleteButton = styled.div`
+  ${flex({ justifyContent: 'space-between', alignItems: 'center' })};
+  cursor: pointer;
+`;
