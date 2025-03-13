11ae64fe7d5ec40dfe2c78af6f0636224088bfe4
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-13 14:40:50 +0900

    feat(admin): 자묻질 디테일 조회 Query hook 추가

--- apps/admin/src/services/faq/queries.ts
+++ apps/admin/src/services/faq/queries.ts
@@ -1,6 +1,6 @@
 import { KEY } from '@/constants/common/constant';
 import { useQuery } from '@tanstack/react-query';
-import { getFaqList } from './api';
+import { getFaq, getFaqList } from './api';
 import { FaqCategory } from '@/types/faq/client';
 
 export const useFaqListQuery = (category: FaqCategory) => {
@@ -10,4 +10,13 @@ export const useFaqListQuery = (category: FaqCategory) => {
   });
 
   return { data: data?.dataList, ...restQuery };
+};
+
+export const useFaqQuery = (id: number) => {
+  const { data, ...restQuery } = useQuery({
+    queryKey: [KEY.FAQ_DETAIL],
+    queryFn: () => getFaq(id),
+  });
+
+  return { data: data, ...restQuery };
 };
