08e8211ef884536d5449a3d5c85184ac654e2817
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-18 15:38:34 +0900

    fix(admin): 변경된 API 반영

--- apps/admin/src/services/admin/api.ts
+++ apps/admin/src/services/admin/api.ts
@@ -3,6 +3,6 @@ import { authorization } from '@/apis/token';
 import { GetAdminRes } from '@/types/admin/remote';
 
 export const getAdmin = async () => {
-  const { data } = await maru.get<GetAdminRes>('/user', authorization());
+  const { data } = await maru.get<GetAdminRes>('/users', authorization());
   return data;
 };
