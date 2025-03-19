81408c028aef2ec4891632ff91579dfb639b4ad8
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-19 11:27:44 +0900

    feat(admin): 입학설명회 리스트 필터 기능 추가

--- apps/admin/src/components/fair/FairList/FairList.tsx
+++ apps/admin/src/components/fair/FairList/FairList.tsx
@@ -2,13 +2,23 @@ import { useFairListQuery } from '@/services/fair/queries';
 import { flex } from '@maru/utils';
 import { styled } from 'styled-components';
 import FairListItem from './FairListItem/FairListItem';
+import FairListHeader from './FairListHeader/FairListHeader';
+import { useState } from 'react';
+import { FAIR_TAP_STATUS } from '@/constants/fair/constant';
 
 const FairList = () => {
+  const [tabStatus, setTabStatus] = useState('진행 중인 신청');
+
   const { data: fairListData } = useFairListQuery();
+
+  const filteredFairList = fairListData?.filter(({ status }) =>
+    FAIR_TAP_STATUS[tabStatus]?.includes(status)
+  );
 
   return (
     <StyledFairList>
-      {fairListData?.map(
+      <FairListHeader selectedTab={tabStatus} handleTabClick={setTabStatus} />
+      {filteredFairList?.map(
         ({ id, start, place, applicationStartDate, applicationEndDate, status }) => (
           <FairListItem
             key={id}
