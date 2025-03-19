6cbea353e164d68b0b14333447bd57a757af9b06
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-19 14:13:24 +0900

    feat(admin): 입학설명회 디테일 테이블 아이템 추가

---
+++ apps/admin/src/components/fair/FairTable/FairTableItem/FairTableItem.tsx
@@ -0,0 +1,52 @@
+import { TableItem } from '@/components/common';
+import { formatPhoneNumber } from '@/utils';
+import { Row, Text } from '@maru/ui';
+
+interface FairTableItemProps {
+  id: number;
+  schoolName: string;
+  name: string;
+  type: string;
+  phoneNumber: string;
+  headcount: number;
+  question: string;
+}
+
+const FairTableItem = ({
+  id,
+  schoolName,
+  name,
+  type,
+  phoneNumber,
+  headcount,
+  question,
+}: FairTableItemProps) => {
+  return (
+    <TableItem key={id}>
+      <Row gap={48}>
+        <Text fontType="p2" width={60}>
+          {name}
+        </Text>
+        <Text fontType="p2" width={60}>
+          {headcount}
+        </Text>
+        <Text fontType="p2" width={60}>
+          {type}
+        </Text>
+        <Text fontType="p2" width={293}>
+          {schoolName}
+        </Text>
+      </Row>
+      <Row gap={48}>
+        <Text fontType="p2" width={120}>
+          {formatPhoneNumber(phoneNumber)}
+        </Text>
+        <Text fontType="p2" width={120}>
+          {question}
+        </Text>
+      </Row>
+    </TableItem>
+  );
+};
+
+export default FairTableItem;
