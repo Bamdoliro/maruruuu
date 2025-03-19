482601e1e1c40b5f82ace34511a72ae32af1cb48
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-19 11:43:37 +0900

    feat(admin): 입학설명회 페이지 추가

--- apps/admin/src/app/fair/page.tsx
+++ apps/admin/src/app/fair/page.tsx
@@ -1,10 +1,28 @@
+'use client';
+
+import FairList from '@/components/fair/FairList/FairList';
 import AppLayout from '@/layouts/AppLayout';
+import { Text } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { styled } from 'styled-components';
 
 const FairPage = () => {
   return (
     <AppLayout>
-      <div>입학설명회관리 페이지 에요.</div>
+      <StyledFairPage>
+        <Text fontType="H1">입학전형 설명회 관리</Text>
+        <FairList />
+      </StyledFairPage>
     </AppLayout>
   );
 };
 export default FairPage;
+
+const StyledFairPage = styled.div`
+  position: relative;
+  ${flex({ flexDirection: 'column' })}
+  gap: 65px;
+  width: 100%;
+  min-height: 100vh;
+  padding: 64px 60px 0 60px;
+`;
