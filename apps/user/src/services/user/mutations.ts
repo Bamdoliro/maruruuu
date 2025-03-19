fdb54dd3ec957c2bc409d65b8323d9e30e3d8ae5
junghongseop <a01082372487@gmail.com>
2025-03-20 08:37:21 +0900

    fix(user): 토스트 문구 추가

--- apps/user/src/services/user/mutations.ts
+++ apps/user/src/services/user/mutations.ts
@@ -3,17 +3,20 @@ import { useRouter } from 'next/navigation';
 import { deleteUser } from './api';
 import { toast } from 'react-toastify';
 import { ROUTES } from '@/constants/common/constants';
+import { useApiError } from '@/hooks';
 
 export const useWithdrawalMutation = (password: string) => {
   const router = useRouter();
+  const { handleError } = useApiError();
 
   const { mutate: withdrawalMutate, ...restMutation } = useMutation({
     mutationFn: () => deleteUser(password),
     onSuccess: () => {
-      toast('');
+      toast('회원탈퇴가 성공되었습니다.', { type: 'success' });
+      localStorage.clear();
       router.replace(ROUTES.MAIN);
     },
-    onError: () => {},
+    onError: handleError,
   });
 
   return { withdrawalMutate, restMutation };
