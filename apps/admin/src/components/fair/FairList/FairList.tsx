f2c234919aeee09d66c73e21384369cb3b46f9e2
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-20 09:37:10 +0900

    style(admin): FairListItem의 크기로 화면 밖으로 넘치던 상황 해결

--- apps/admin/src/components/fair/FairList/FairList.tsx
+++ apps/admin/src/components/fair/FairList/FairList.tsx
@@ -1,10 +1,10 @@
 import { useFairListQuery } from '@/services/fair/queries';
-import { flex } from '@maru/utils';
 import { styled } from 'styled-components';
 import FairListItem from './FairListItem/FairListItem';
 import FairListHeader from './FairListHeader/FairListHeader';
 import { useState } from 'react';
 import { FAIR_TAP_STATUS } from '@/constants/fair/constant';
+import { Column } from '@maru/ui';
 
 const FairList = () => {
   const [tabStatus, setTabStatus] = useState('진행 중인 신청');
@@ -16,30 +16,31 @@ const FairList = () => {
   );
 
   return (
-    <StyledFairList>
+    <Column width="100%" gap={64}>
       <FairListHeader selectedTab={tabStatus} handleTabClick={setTabStatus} />
-      {filteredFairList?.map(
-        ({ id, start, place, applicationStartDate, applicationEndDate, status }) => (
-          <FairListItem
-            key={id}
-            id={id}
-            start={start}
-            place={place}
-            applicationStartDate={applicationStartDate}
-            applicationEndDate={applicationEndDate}
-            status={status}
-          />
-        )
-      )}
-    </StyledFairList>
+      <StyledFairList>
+        {filteredFairList?.map(
+          ({ id, start, place, applicationStartDate, applicationEndDate, status }) => (
+            <FairListItem
+              key={id}
+              id={id}
+              start={start}
+              place={place}
+              applicationStartDate={applicationStartDate}
+              applicationEndDate={applicationEndDate}
+              status={status}
+            />
+          )
+        )}
+      </StyledFairList>
+    </Column>
   );
 };
 
 export default FairList;
 
 const StyledFairList = styled.div`
-  ${flex({ alignItems: 'flex-start' })}
-  align-content: flex-start;
-  gap: 16px;
-  flex-wrap: wrap;
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 400px);
+  gap: 32px;
 `;
