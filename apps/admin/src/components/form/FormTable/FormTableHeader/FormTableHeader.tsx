7c69d49fea0e5de7f20ecbf99480ba23892761a9
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-24 10:05:39 +0900

    feat(admin): FormTable에 반응형 적용

--- apps/admin/src/components/form/FormTable/FormTableHeader/FormTableHeader.tsx
+++ apps/admin/src/components/form/FormTable/FormTableHeader/FormTableHeader.tsx
@@ -1,37 +1,38 @@
 import TableHeader from '@/components/common/TableHeader/TableHeader';
+import { convertToResponsive } from '@/utils';
 import { Row, Text } from '@maru/ui';
 
 const FormTableHeader = () => {
   return (
     <TableHeader>
       <Row gap={48}>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           번호
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           번호
         </Text>
-        <Text fontType="p2" width={160}>
+        <Text fontType="p2" width={convertToResponsive(120, 160)}>
           번호
         </Text>
-        <Text fontType="p2" width={240}>
+        <Text fontType="p2" width={convertToResponsive(180, 240)}>
           제목
         </Text>
       </Row>
       <Row gap={48}>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           제출상태
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           제출서류
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           1차 결과
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           최종 점수
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           2차 결과
         </Text>
       </Row>
--- apps/admin/src/components/form/FormTable/FormTableItem/FormTableItem.tsx
+++ apps/admin/src/components/form/FormTable/FormTableItem/FormTableItem.tsx
@@ -1,37 +1,64 @@
 import { TableItem } from '@/components/common';
+import { convertToResponsive } from '@/utils';
 import { Row, Text } from '@maru/ui';
 
-const FormTableItem = () => {
+interface FormTableItemProps {
+  id: number;
+  examinationNumber: number | null;
+  name: string;
+  graduationType: string; //GraduationType;
+  school: string;
+  status: string; //FormStatus;
+  type: string; //FormType;
+  isChangedToRegular: boolean;
+  totalScore: number | null;
+  firstRoundPassed: boolean | null;
+  secondRoundPassed: boolean | null;
+}
+
+const FormTableItem = ({
+  id,
+  examinationNumber,
+  name,
+  school,
+  graduationType,
+  status,
+  type,
+  isChangedToRegular,
+  totalScore,
+  firstRoundPassed,
+  secondRoundPassed,
+}: FormTableItemProps) => {
   return (
-    <TableItem key={1}>
+    <TableItem key={id}>
       <Row gap={48}>
-        <Text fontType="p2" width={60}>
-          1001
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
+          {examinationNumber}
         </Text>
-        <Text fontType="p2" width={60}>
-          이민준
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
+          {name}
         </Text>
-        <Text fontType="p2" width={160}>
-          재송중학교
+        <Text fontType="p2" width={convertToResponsive(120, 160)}>
+          {school}
         </Text>
-        <Text fontType="p2" width={240}>
-          일반
+        <Text fontType="p2" width={convertToResponsive(180, 240)}>
+          {graduationType}
         </Text>
       </Row>
       <Row gap={48}>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           최종 제출
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           승인
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           합격
         </Text>
-        <Text fontType="p2" width={60}>
-          220.00
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
+          {typeof totalScore !== 'number' ? '미정' : Number(totalScore.toFixed(3))}
         </Text>
-        <Text fontType="p2" width={60}>
+        <Text fontType="p2" width={convertToResponsive(40, 60)}>
           합격
         </Text>
       </Row>
