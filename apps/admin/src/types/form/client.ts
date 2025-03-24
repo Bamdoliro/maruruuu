9921a9a338327d70bb2718bafa0853383f434ae7
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-24 10:13:19 +0900

    feat(admin): FromStatus type 추가

---
+++ apps/admin/src/types/form/client.ts
@@ -0,0 +1,12 @@
+export type FormStatus =
+  | 'RECEIVED'
+  | 'FIRST_FAILED'
+  | 'FAILED'
+  | 'FINAL_SUBMITTED'
+  | 'SUBMITTED'
+  | 'APPROVED'
+  | 'NO_SHOW'
+  | 'FIRST_PASSED'
+  | 'PASSED'
+  | 'REJECTED'
+  | 'ENTERED';
