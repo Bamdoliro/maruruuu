38ee191cb479e9f36ca260c322675cb211aa723e
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-03-25 09:14:58 +0900

    feat(admin): 검토해야 하는 원서 및 상태, 특정, 정렬 원서 조회 API 연동

--- apps/admin/src/services/form/queries.ts
+++ apps/admin/src/services/form/queries.ts
@@ -1,11 +1,18 @@
 import { KEY } from '@/constants/common/constant';
 import { getFormList } from './api';
 import { useQuery } from '@tanstack/react-query';
+import {
+  useFormListSortingTypeValueStore,
+  useFormListTypeValueStore,
+} from '@/store/form/formType';
 
 export const useFormListQuery = () => {
+  const formListType = useFormListTypeValueStore();
+  const formListSortingType = useFormListSortingTypeValueStore();
+
   const { data, ...restQuery } = useQuery({
-    queryKey: [KEY.FORM_LIST],
-    queryFn: getFormList,
+    queryKey: [KEY.FORM_LIST, formListType, formListSortingType],
+    queryFn: () => getFormList(formListType, formListSortingType),
   });
 
   return { data: data?.dataList, ...restQuery };
