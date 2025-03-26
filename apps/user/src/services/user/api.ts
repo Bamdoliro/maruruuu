842f113ec335ce5454a22468596ea25c8e5c1af8
junghongseop <a01082372487@gmail.com>
2025-03-26 14:03:48 +0900

    feat(user): 회원가입, 인증번호 요청, 인증 api 연동

--- apps/user/src/services/user/api.ts
+++ apps/user/src/services/user/api.ts
@@ -1,6 +1,11 @@
 import authorization from '@/apis/authorization/authorization';
 import { maru } from '@/apis/instance/instance';
-import { GetUserRes } from '@/types/user/remote';
+import {
+  GetUserRes,
+  PatchUserVerificationReq,
+  PostSignUpReq,
+  PostUserVerificationReq,
+} from '@/types/user/remote';
 
 export const getUser = async () => {
   const { data } = await maru.get<GetUserRes>('/users', authorization());
@@ -10,6 +15,31 @@ export const getUser = async () => {
 
 export const deleteUser = async (password: string) => {
   const { data } = await maru.delete('/users', { data: password, ...authorization() });
+
+  return data;
+};
+
+export const postSignUp = async ({ phoneNumber, name, password }: PostSignUpReq) => {
+  const { data } = await maru.post('/users', { phoneNumber, name, password });
+
+  return data;
+};
+
+export const postUserVerification = async ({
+  phoneNumber,
+  type,
+}: PostUserVerificationReq) => {
+  const { data } = await maru.post('/users/verification', { phoneNumber, type });
+
+  return data;
+};
+
+export const patchUserVerification = async ({
+  phoneNumber,
+  type,
+  code,
+}: PatchUserVerificationReq) => {
+  const { data } = await maru.patch('/users/verification', { phoneNumber, type, code });
 
   return data;
 };
--- apps/user/src/types/user/remote.ts
+++ apps/user/src/types/user/remote.ts
@@ -1,5 +1,25 @@
+import { postUserVerification } from './../../services/user/api';
 import type { User } from './client';
 
 export interface GetUserRes {
   data: User;
 }
+
+export interface PostSignUpReq {
+  phoneNumber: string;
+  name: string;
+  password: string;
+}
+
+export interface PostUserVerificationReq {
+  phoneNumber: string;
+  type: Type;
+}
+
+export interface PatchUserVerificationReq {
+  phoneNumber: string;
+  code: string;
+  type: Type;
+}
+
+export type Type = 'SIGNUP' | 'UPDATE_PASSWORD';
