7bb245b5098c1c7296b75469e1fbef5ec28722c3
junghongseop <a01082372487@gmail.com>
2025-03-27 08:01:31 +0900

    fix(user): checked, onChange 함수 추가 및 중복 ui 코드 개선

---
+++ apps/user/src/components/signup/Terms/Terms.hook.ts
@@ -0,0 +1,26 @@
+import { Dispatch, SetStateAction, useState } from 'react';
+
+export const useCheckTerms = (setTermsAgree: Dispatch<SetStateAction<boolean>>) => {
+  const [checks, setChecks] = useState([false, false, false]);
+
+  const handleCheck = (index?: number) => {
+    if (index === undefined) {
+      const allChecked = !checks.every(Boolean);
+      setChecks(Array(3).fill(allChecked));
+      setTermsAgree(allChecked);
+    } else {
+      const newChecks = checks.map((c, i) => (i === index ? !c : c));
+      const allChecked = newChecks.every(Boolean);
+      setChecks(newChecks);
+      setTermsAgree(allChecked);
+    }
+  };
+
+  return {
+    checks,
+    handleCheckAll: () => handleCheck(),
+    handleCheckOne: () => handleCheck(0),
+    handleCheckTwo: () => handleCheck(1),
+    handleCheckThree: () => handleCheck(2),
+  };
+};
--- apps/user/src/components/signup/Terms/Terms.tsx
+++ apps/user/src/components/signup/Terms/Terms.tsx
@@ -3,40 +3,62 @@ import { flex } from '@maru/utils';
 import styled from 'styled-components';
 import TermsItem from './TermsItem/TermsItem';
 import { ROUTES } from '@/constants/common/constants';
+import { Dispatch, SetStateAction } from 'react';
+import { useCheckTerms } from './Terms.hook';
 
-const Terms = () => {
+interface TermsProps {
+  setTermsAgree: Dispatch<SetStateAction<boolean>>;
+}
+
+const Terms = ({ setTermsAgree }: TermsProps) => {
+  const { checks, handleCheckAll, handleCheckOne, handleCheckTwo, handleCheckThree } =
+    useCheckTerms(setTermsAgree);
+
+  const terms = [
+    {
+      id: 'agreement1',
+      checked: checks[0],
+      onChange: handleCheckOne,
+      link: ROUTES.TERMS_OF_SERVICE,
+      label: '이용약관 동의',
+    },
+    {
+      id: 'agreement2',
+      checked: checks[1],
+      onChange: handleCheckTwo,
+      link: ROUTES.PERSONAL_INFO_COLLECTION,
+      label: '개인정보 수집 이용동의',
+    },
+    {
+      id: 'agreement3',
+      checked: checks[2],
+      onChange: handleCheckThree,
+      link: ROUTES.PRIVACY_POLCY,
+      label: '개인정보 처리 방침 동의',
+    },
+  ];
+
   return (
     <StyledTerms>
       <TermsItem
         id="all"
-        checked={false}
-        onChange={() => {}}
+        checked={checks.every(Boolean)}
+        onChange={handleCheckAll}
         link={ROUTES.TERMS_OF_SERVICE}
         label="약관 전체동의"
         enabled={false}
       />
       <hr />
-      <TermsItem
-        id="agreement1"
-        checked={false}
-        onChange={() => {}}
-        link={ROUTES.TERMS_OF_SERVICE}
-        label="이용약관 동의"
-      />
-      <TermsItem
-        id="agreement2"
-        checked={false}
-        onChange={() => {}}
-        link={ROUTES.PERSONAL_INFO_COLLECTION}
-        label="개인정보 수집 이용동의"
-      />
-      <TermsItem
-        id="agreement1"
-        checked={false}
-        onChange={() => {}}
-        link={ROUTES.PRIVACY_POLCY}
-        label="개인정보 처리 방침 동의"
-      />
+      {terms.map(({ id, checked, onChange, link, label }) => (
+        <TermsItem
+          key={id}
+          id={id}
+          checked={checked}
+          onChange={onChange}
+          link={link}
+          label={label}
+        />
+      ))}
     </StyledTerms>
   );
 };
