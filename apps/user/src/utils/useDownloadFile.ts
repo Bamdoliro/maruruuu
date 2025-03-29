0a23e71999b1ee7bef4e1d7cd6fe5cdefefd2334
junghongseop <a01082372487@gmail.com>
2025-03-29 19:46:25 +0900

    fix(user): 파일 다운로드 유틸리티 수정

--- apps/user/src/utils/useDownloadFile.ts
+++ apps/user/src/utils/useDownloadFile.ts
@@ -1,14 +1,13 @@
-const useDownloadFile = (data: Blob | string | undefined, fileName: string) => {
+const useDownloadFile = (data: string | undefined, fileName: string) => {
   if (!data) return;
 
-  const fileUrl = window.URL.createObjectURL(new Blob([data]));
   const link = document.createElement('a');
-  link.href = fileUrl;
-  link.download = fileName;
+  link.href = data;
+  link.setAttribute('download', fileName);
   document.body.appendChild(link);
   link.click();
   link.remove();
-  window.URL.revokeObjectURL(fileUrl);
+  window.URL.revokeObjectURL(data);
 };
 
 export default useDownloadFile;
