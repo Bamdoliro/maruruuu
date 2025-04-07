bc9e04eca1be4f62fefdd491016de71eeb127b12
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-07 15:28:07 +0900

    feat(admin): 2차 합격 여부 변경 UI 추가

--- apps/admin/src/components/form/FormTable/FormTableItem/FormTableItem.tsx
+++ apps/admin/src/components/form/FormTable/FormTableItem/FormTableItem.tsx
@@ -1,9 +1,11 @@
 import { TableItem } from '@/components/common';
 import { FORM_TYPE_CATEGORY } from '@/constants/form/constant';
-import { Form } from '@/types/form/client';
+import { useIsSecondRoundResultEditingValueStore } from '@/store/form/isSecondRoundResultEditing';
+import { useSecondRoundResultStore } from '@/store/form/secondRoundResult';
+import { Form, PassStatusType } from '@/types/form/client';
 import { convertToResponsive } from '@/utils';
 import { color } from '@maru/design-system';
-import { Row, Text } from '@maru/ui';
+import { Dropdown, Row, Text } from '@maru/ui';
 
 const FormTableItem = ({
   id,
@@ -17,6 +19,16 @@ const FormTableItem = ({
   firstRoundPassed,
   secondRoundPassed,
 }: Form) => {
+  const isSecondRoundResultEditing = useIsSecondRoundResultEditingValueStore();
+  const [secondRoundResult, setSecondRoundResult] = useSecondRoundResultStore();
+
+  const handleSecondPassResultDropdownChange = (value: string) => {
+    setSecondRoundResult((prev) => ({
+      ...prev,
+      [id]: value as PassStatusType,
+    }));
+  };
+
   const getStatusColor = (status: boolean | null) => {
     if (status === null) return color.gray600;
     return status ? color.maruDefault : color.red;
@@ -45,7 +57,7 @@ const FormTableItem = ({
           {FORM_TYPE_CATEGORY[type]}
         </Text>
       </Row>
-      <Row gap={48}>
+      <Row gap={48} justify-content="flex-end">
         <Text fontType="p2" width={convertToResponsive(40, 60)}>
           {status === 'SUBMITTED' ? '초안 제출' : '최종 제출'}
         </Text>
@@ -66,13 +78,26 @@ const FormTableItem = ({
         >
           {typeof totalScore !== 'number' ? '미정' : Number(totalScore.toFixed(3))}
         </Text>
-        <Text
-          fontType="p2"
-          width={convertToResponsive(40, 60)}
-          color={getStatusColor(secondRoundPassed)}
-        >
-          {getRoundResult(secondRoundPassed, status)}
-        </Text>
+      </Row>
+      <Row>
+        {isSecondRoundResultEditing ? (
+          <Dropdown
+            name="pass"
+            size="SMALL"
+            width={80}
+            value={secondRoundResult[id] || getRoundResult(secondRoundPassed, status)}
+            data={['합격', '불합격']}
+            onChange={handleSecondPassResultDropdownChange}
+          />
+        ) : (
+          <Text
+            fontType="p2"
+            width={convertToResponsive(40, 60)}
+            color={getStatusColor(secondRoundPassed)}
+          >
+            {getRoundResult(secondRoundPassed, status)}
+          </Text>
+        )}
       </Row>
     </TableItem>
   );
