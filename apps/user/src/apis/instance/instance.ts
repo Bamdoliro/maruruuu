abbe9b82d0c419b57047b3278b79a5fe4ee0d6b3
junghongseop <a01082372487@gmail.com>
2025-04-09 09:54:30 +0900

    fix(user): 불필요한 코드 삭제

--- apps/user/src/apis/instance/instance.ts
+++ apps/user/src/apis/instance/instance.ts
@@ -1,5 +1,4 @@
 import axios from 'axios';
-import refreshToken from '../token/refresh';
 
 export const maru = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BASE_URL,
@@ -14,36 +13,6 @@ maru.interceptors.request.use(
     return config;
   },
   (error) => {
-    return Promise.reject(error);
-  }
-);
-
-maru.interceptors.response.use(
-  (response) => {
-    return response;
-  },
-  async (error) => {
-    if (error.response) {
-      const {
-        status,
-        data: { message, code },
-      } = error.response;
-
-      if (message) {
-        if (status === 401 && code === 'EXPIRED_TOKEN') {
-          try {
-            const newToken = await refreshToken();
-
-            maru.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
-
-            error.config.headers['Authorization'] = `Bearer ${newToken}`;
-            return maru(error.config);
-          } catch (refreshError) {
-            return Promise.reject(refreshError);
-          }
-        }
-      }
-    }
     return Promise.reject(error);
   }
 );
