952c053d664d65a4cd3ee7562b2d09f2cd0e98b5
junghongseop <a01082372487@gmail.com>
2025-04-09 11:49:53 +0900

    fix(user): AleryStyleModal의 onClose 수정

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -33,8 +33,8 @@ export default function Home() {
           title={message}
           isOpen={isOpen}
           onClose={() => {
-            handleClose();
             close();
+            handleClose();
           }}
         />
       ));
