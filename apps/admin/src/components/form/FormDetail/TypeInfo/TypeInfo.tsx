99f0d8ffffd97c5fd4a4fd24fe325ce3a3d0f2e2
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-11 08:36:35 +0900

    feat(admin): 전형정보 퍼블리싱

---
+++ apps/admin/src/components/form/FormDetail/TypeInfo/TypeInfo.tsx
@@ -0,0 +1,33 @@
+import { DataBox } from '@/components/common';
+import { Loader } from '@maru/ui';
+import { flex } from '@maru/utils';
+import { styled } from 'styled-components';
+
+interface TypeInfoProps {
+  type: string;
+}
+
+const TypeInfo = ({ type }: TypeInfoProps) => {
+  if (!type) return <Loader />;
+
+  return (
+    <StyledTypeInfo>
+      <GridContainer></GridContainer>
+    </StyledTypeInfo>
+  );
+};
+export default TypeInfo;
+
+const StyledTypeInfo = styled.div`
+  ${flex({ flexDirection: 'column' })}
+  padding: 48px 0;
+  gap: 24px;
+`;
+
+const GridContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  gap: 24px;
+`;
+
+const GridItem = styled.div``;
