e50e05be04cc4215547b703a1e34d25017ce13e3
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-10 22:47:22 +0900

    feat(admin): 출신학교 및 학력 데이터 동적 렌더링

--- apps/admin/src/components/form/FormDetail/EducationInfo/EducationInfo.tsx
+++ apps/admin/src/components/form/FormDetail/EducationInfo/EducationInfo.tsx
@@ -18,9 +18,27 @@ interface EducationInfoProps {
 
 const EducationInfo = ({ educationData }: EducationInfoProps) => {
   if (!educationData) return <Loader />;
+
+  const educationDetails = [
+    { label: '졸업 구분', data: educationData.graduationType },
+    { label: '출신 학교명', data: educationData.schoolName },
+    { label: '졸업년도, 합격년도', data: educationData.graduationYear },
+    { label: '학교 지역', data: educationData.schoolLocation },
+    { label: '표준학교코드', data: educationData.schoolCode },
+    { label: '학교 연락처', data: educationData.teacherPhoneNumber },
+    { label: '작성교사 이름', data: educationData.teacherName },
+    { label: '작성교사 연락처', data: educationData.teacherMobilePhoneNumber },
+  ];
+
   return (
     <StyledEducationInfo>
-      <GridContainer></GridContainer>
+      <GridContainer>
+        {educationDetails.map((item, index) => (
+          <GridItem key={index}>
+            <DataBox label={item.label} data={item.data} />
+          </GridItem>
+        ))}
+      </GridContainer>
     </StyledEducationInfo>
   );
 };
