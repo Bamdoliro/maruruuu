74f906cefe0261f271805c5c6280087f2fbfe120
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-10 14:27:09 +0900

    feat(admin): 증명 사진 추가

--- apps/admin/src/components/form/FormDetail/ApplicantInfo/ApplicantInfo.tsx
+++ apps/admin/src/components/form/FormDetail/ApplicantInfo/ApplicantInfo.tsx
@@ -1,6 +1,8 @@
 import { DataBox } from '@/components/common';
-import { Loader } from '@maru/ui';
+import { color } from '@maru/design-system';
+import { Loader, Text } from '@maru/ui';
 import { flex } from '@maru/utils';
+import Image from 'next/image';
 import { styled } from 'styled-components';
 
 interface ApplicantInfoProps {
@@ -31,6 +33,18 @@ const ApplicantInfo = ({ applicantData }: ApplicantInfoProps) => {
             <DataBox label={item.label} data={item.data} />
           </GridItem>
         ))}
+        <ProfileImageBox>
+          <Text fontType="H4" color={color.gray900}>
+            증명사진
+          </Text>
+          <ProfileImage
+            src={applicantData.profileImageUrl}
+            alt="profile-image"
+            width={280}
+            height={354}
+            style={{ objectFit: 'cover', objectPosition: 'top' }}
+          />
+        </ProfileImageBox>
       </GridContainer>
     </StyledApplicantInfo>
   );
@@ -50,3 +64,19 @@ const GridContainer = styled.div`
 `;
 
 const GridItem = styled.div``;
+
+const ProfileImageBox = styled.div`
+  ${flex({ flexDirection: 'column', alignItems: 'flex-start' })}
+  width: 100%;
+  min-width: 400px;
+  padding: 24px;
+  gap: 16px;
+
+  border-radius: 12px;
+  border: 1px solid ${color.gray200};
+  background: ${color.white};
+`;
+
+const ProfileImage = styled(Image)`
+  background: ${color.gray100};
+`;
