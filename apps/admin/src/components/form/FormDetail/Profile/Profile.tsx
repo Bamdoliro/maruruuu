356dbde5c943ded15e52e2d869066592bd06ffeb
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-10 10:11:51 +0900

    feat(admin): 프로필에 필요한 데이터 Props

--- apps/admin/src/components/form/FormDetail/Profile/Profile.tsx
+++ apps/admin/src/components/form/FormDetail/Profile/Profile.tsx
@@ -4,15 +4,32 @@ import { flex } from '@maru/utils';
 import Image from 'next/image';
 import { styled } from 'styled-components';
 
-const Profile = () => {
+interface ProfileProps {
+  profileData: {
+    profileImageUrl: string;
+    name: string;
+    phoneNumber: string;
+    examinationNumber: number;
+    type: string;
+    schoolName: string;
+  };
+}
+
+const Profile = ({ profileData }: ProfileProps) => {
   return (
     <StyledProfile>
       <ProfileImageBox>
-        <ProfileImage />
+        <ProfileImage
+          src={profileData.profileImageUrl}
+          alt="profile-image"
+          width={280}
+          height={280}
+          style={{ objectFit: 'cover', objectPosition: 'top' }}
+        />
       </ProfileImageBox>
       <Column gap={16}>
         <Text fontType="H2" color={color.gray900}>
-          {'이름'}
+          {profileData.name}
         </Text>
         <Column gap={8}>{`번호, 전형, 학교, 전번`}</Column>
       </Column>
