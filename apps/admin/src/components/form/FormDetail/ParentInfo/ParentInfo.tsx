23919ee124c79ffd0d4582523399a4ad9511f315
Minjun Lee <127070837+MinjuN07@users.noreply.github.com>
2025-04-10 20:10:44 +0900

    feat(admin): 보호자정보 데이터 동적 렌더링

--- apps/admin/src/components/form/FormDetail/ParentInfo/ParentInfo.tsx
+++ apps/admin/src/components/form/FormDetail/ParentInfo/ParentInfo.tsx
@@ -1,4 +1,5 @@
 import { DataBox } from '@/components/common';
+import { Loader } from '@maru/ui';
 import { flex } from '@maru/utils';
 import { styled } from 'styled-components';
 
@@ -8,15 +9,32 @@ interface ParentInfoProps {
     phoneNumber: string;
     relation: string;
     address: string;
-    zoneCode: string;
     detailAddress: string;
+    zoneCode: string;
   };
 }
 
 const ParentInfo = ({ parentData }: ParentInfoProps) => {
+  if (!parentData) return <Loader />;
+
+  const parentDetails = [
+    { label: '이름', data: parentData.name },
+    { label: '전화번호', data: parentData.phoneNumber },
+    { label: '보호자 관계', data: parentData.relation },
+    { label: '주소', data: parentData.address },
+    { label: '상세 주소', data: parentData.detailAddress },
+    { label: '우편 번호', data: parentData.zoneCode },
+  ];
+
   return (
     <StyledParentInfo>
-      <GridContainer></GridContainer>
+      <GridContainer>
+        {parentDetails.map((item, index) => (
+          <GridItem key={index}>
+            <DataBox label={item.label} data={item.data} />
+          </GridItem>
+        ))}
+      </GridContainer>
     </StyledParentInfo>
   );
 };
