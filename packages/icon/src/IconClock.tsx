b594d641bc35a18b0c468565475b18a67042f983
hwangjiyeong916 <bomaye119@gmail.com>
2025-04-12 17:32:03 +0900

    fix(admin): svg props 지정 오류 수정

--- packages/icon/src/IconClock.tsx
+++ packages/icon/src/IconClock.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
-const SvgIconClock = (props) => (
+import type { SVGProps } from 'react';
+const SvgIconClock = (props: SVGProps<SVGSVGElement>) => (
   <svg xmlns="http://www.w3.org/2000/svg" width={24} height={24} fill="none" {...props}>
     <path
       fill="#9497A0"
