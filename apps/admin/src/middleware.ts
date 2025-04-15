1ce6dc2556ed62e228e9725037c14ee8db767e09
oxjadex <oharin765@gmail.com>
2025-04-15 11:30:15 +0900

    chore(admin): 줄 수정

--- apps/admin/src/middleware.ts
+++ apps/admin/src/middleware.ts
@@ -2,18 +2,19 @@ import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-  const response = NextResponse.next();
   const cookies = request.headers.get('cookie');
   const accessToken = cookies
     ?.split('; ')
     .find((row) => row.startsWith('refresh-token='))
     ?.split('=')[1];
+
   if (!accessToken) {
     const redirectUrl = new URL('/', request.url);
     redirectUrl.searchParams.set('message', '로그인 후 시도해주세요');
     return NextResponse.redirect(redirectUrl);
   }
-  return response;
+
+  return NextResponse.next();
 };
 
 export const config = {
