42239502a12e0bbb4ddc0b205a461773c97a72f7
junghongseop <a01082372487@gmail.com>
2025-04-28 08:34:41 +0900

    fix(user): 사용하지 않는 props 지움

--- apps/user/src/app/form/DraftCompleted/DraftCompleted.tsx
+++ apps/user/src/app/form/DraftCompleted/DraftCompleted.tsx
@@ -2,22 +2,14 @@
 
 import { CompleteAlarm, DraftCompletedContent, DraftFormModal } from '@/components/form';
 import { AppLayout } from '@/layouts';
-import { IconCancelCircle, IconLoader } from '@maru/icon';
+import { IconLoader } from '@maru/icon';
 import styled from 'styled-components';
-import { useCheckFilledForm, useCTAButton } from './DraftCompleted.hook';
+import { useCTAButton } from './DraftCompleted.hook';
 import { useOverlay } from '@toss/use-overlay';
 
 const DraftCompleted = () => {
   const overlay = useOverlay();
   const { handleCheckAgainForm, handleSubmitDraftForm } = useCTAButton();
-  const {
-    applicantFilledCount,
-    parentFilledCount,
-    educationFilledCount,
-    typeFilledCount,
-    documentFilledCount,
-    isFilledForm: isComplete,
-  } = useCheckFilledForm();
 
   const openDraftFormModal = () => {
     overlay.open(({ isOpen, close }) => (
@@ -32,29 +24,15 @@ const DraftCompleted = () => {
     ));
   };
 
-  console.log(isComplete);
-
   return (
     <AppLayout header>
       <CompleteAlarmBox>
         <CompleteAlarm
-          isComplete={isComplete}
           completeText="입학 원서 작성 중"
           completeIcon={<IconLoader width={150} height={150} />}
-          inCompleteText="아직 작성하지 않은 곳이 있어요"
-          inCompleteIcon={<IconCancelCircle width={150} height={150} />}
         />
       </CompleteAlarmBox>
-      <DraftCompletedContent
-        isComplete={isComplete}
-        applicantFilledCount={applicantFilledCount}
-        parentFilledCount={parentFilledCount}
-        educationFilledCount={educationFilledCount}
-        typeFilledCount={typeFilledCount}
-        documentFilledCount={documentFilledCount}
-        onClick={handleCheckAgainForm}
-        check={openDraftFormModal}
-      />
+      <DraftCompletedContent onClick={handleCheckAgainForm} check={openDraftFormModal} />
     </AppLayout>
   );
 };
