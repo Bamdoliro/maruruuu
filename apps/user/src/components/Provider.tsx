cb75c69acc06f4c235fa1e8251a72c3f7d853db6
junghongseop <a01082372487@gmail.com>
2025-07-17 00:16:31 +0900

    fix(user): 글로벌 토스트 컴포넌트 프로바이더 개발

--- apps/user/src/components/Provider.tsx
+++ apps/user/src/components/Provider.tsx
@@ -1,15 +1,22 @@
 'use client';
 
 import { GlobalStyle } from '@maru/design-system';
-import { Loader } from '@maru/ui';
+import { Loader, Toast } from '@maru/ui';
 import { OverlayProvider } from '@toss/use-overlay';
 import type { ReactNode } from 'react';
 import { RecoilRoot } from 'recoil';
 import { Suspense } from 'react';
+import { useToast } from '@/hooks';
 
 interface Props {
   children: ReactNode;
 }
+
+const GlobalToast = () => {
+  const { showToast, toastMessage, toastType } = useToast();
+
+  return showToast ? <Toast type={toastType}>{toastMessage}</Toast> : null;
+};
 
 const Provider = ({ children }: Props) => {
   return (
@@ -18,6 +25,7 @@ const Provider = ({ children }: Props) => {
         <GlobalStyle />
         <Suspense fallback={<Loader />}>{children}</Suspense>
       </OverlayProvider>
+      <GlobalToast />
     </RecoilRoot>
   );
 };
