b9535e9e7af0895dc6d8c0b711583a4b239308bc
junghongseop <a01082372487@gmail.com>
2025-07-19 21:58:01 +0900

    fix(user): 제출할때 프로필 상태관리 빈문자열 변경되게 수정

--- apps/user/src/app/form/DraftCompleted/DraftCompleted.hook.ts
+++ apps/user/src/app/form/DraftCompleted/DraftCompleted.hook.ts
@@ -4,9 +4,11 @@ import {
 } from '@/services/form/mutations';
 import { useFormStatusQuery } from '@/services/form/queries';
 import { useFormValueStore, useSetFormStepStore } from '@/stores';
+import { useSetProfileStore } from '@/stores/form/profile';
 
 export const useCTAButton = () => {
   const form = useFormValueStore();
+  const setProfile = useSetProfileStore();
   const setFormStep = useSetFormStepStore();
   const { submitDraftFormMutate } = useSubmitDraftFormMutation(form);
   const { correctionFormMutate } = useCorrectionFormMutation();
@@ -19,8 +21,10 @@ export const useCTAButton = () => {
   const handleSubmitDraftForm = () => {
     if (statusData?.status === 'REJECTED') {
       correctionFormMutate(form);
+      setProfile({ fileName: '' });
     } else {
       submitDraftFormMutate();
+      setProfile({ fileName: '' });
     }
   };
 
