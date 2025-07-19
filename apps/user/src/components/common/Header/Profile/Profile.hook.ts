0e7d2facc60b322a7b86ea30b37a564bcc505510
junghongseop <a01082372487@gmail.com>
2025-07-19 21:58:33 +0900

    fix(user): 상태에 따른 페이지 이동 여부

--- apps/user/src/components/common/Header/Profile/Profile.hook.ts
+++ apps/user/src/components/common/Header/Profile/Profile.hook.ts
@@ -2,11 +2,26 @@ import { ROUTES } from '@/constants/common/constants';
 import { useLogoutMutation } from '@/services/auth/mutations';
 import { useRouter } from 'next/navigation';
 
-export const useCTAButton = () => {
+export const useCTAButton = (status?: string) => {
   const router = useRouter();
   const { logoutMutate } = useLogoutMutation();
 
+  const banStatus =
+    status === 'APPROVED' ||
+    status === 'FINAL_SUBMITTED' ||
+    status === 'PASSED' ||
+    status === 'RECEIVED' ||
+    status === 'FIRST_PASSED' ||
+    status === 'FAILED' ||
+    status === 'FIRST_FAILED' ||
+    status === 'NO_SHOW' ||
+    status === 'ENTERED';
+
   const handleMoveForm = () => {
+    if (banStatus) {
+      alert('현재 상태에서는 원서 작성 페이지로 이동할 수 없습니다.');
+      return;
+    }
     router.push(ROUTES.FORM);
   };
 
