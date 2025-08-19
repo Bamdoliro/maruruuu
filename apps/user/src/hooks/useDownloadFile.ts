d17d5a4094af845c33a7ec16eb0405bd8b4bc72b
sunz8281 <sunz8281@gmail.com>
2025-08-20 08:16:16 +0900

    fix(user): downloadFile callback 적용

--- apps/user/src/hooks/useDownloadFile.ts
+++ apps/user/src/hooks/useDownloadFile.ts
@@ -1,34 +1,39 @@
 import { useToast } from '@/hooks';
+import { useCallback } from 'react';
 
 const useDownloadFile = () => {
   const { toast } = useToast();
 
-  const downloadFile = (data: string | Blob | undefined, fileName: string) => {
-    try {
-      if (!data) return;
+  const downloadFile = useCallback(
+    (data: string | Blob | undefined, fileName: string) => {
+      try {
+        if (!data) return;
 
-      let url: string;
+        let url: string;
 
-      if (data instanceof Blob) {
-        url = window.URL.createObjectURL(data);
-      } else {
-        url = data;
+        if (data instanceof Blob) {
+          url = window.URL.createObjectURL(data);
+        } else {
+          url = data;
+        }
+
+        const link = document.createElement('a');
+        link.href = url;
+        link.setAttribute('download', fileName);
+        document.body.appendChild(link);
+        link.click();
+        document.body.removeChild(link);
+
+        if (data instanceof Blob) {
+          window.URL.revokeObjectURL(url);
+        }
+      } catch (error) {
+        toast('파일 다운로드에 실패하였습니다.', 'ERROR');
       }
+    },
+    [toast]
+  );
 
-      const link = document.createElement('a');
-      link.href = url;
-      link.setAttribute('download', fileName);
-      document.body.appendChild(link);
-      link.click();
-      document.body.removeChild(link);
-
-      if (data instanceof Blob) {
-        window.URL.revokeObjectURL(url);
-      }
-    } catch (error) {
-      toast('파일 다운로드에 실패하였습니다.', 'ERROR');
-    }
-  };
   return downloadFile;
 };
 
