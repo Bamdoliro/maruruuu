c3421e8d1c605b5678d8ad8390705f12738711e7
sunz8281 <sunz8281@gmail.com>
2025-08-23 01:08:45 +0900

    refactor(user): 불필요한 코드 제거
    - useSetmSubjectIncompleteStore
    -

--- apps/user/src/components/form/Calculators/GradeCalculator/BasicCalculator/BasicCalculatorItem/BasicCalculatorItem.hook.ts
+++ apps/user/src/components/form/Calculators/GradeCalculator/BasicCalculator/BasicCalculatorItem/BasicCalculatorItem.hook.ts
@@ -1,19 +1,12 @@
-import { useSetmSubjectIncompleteStore, useSetSubjectListStore } from '@/stores';
-
-const CORE_SUBJECTS = ['국어', '영어', '수학'];
+import { useSetSubjectListStore } from '@/stores';
 
 export const useInput = (id: number) => {
   const setSubjectList = useSetSubjectListStore();
-  const setSubjectIncomplete = useSetmSubjectIncompleteStore();
 
   const handleSubjectChange = (data: string, name: string) => {
-    let isIncomplete = false;
-    let subjectName = '';
     setSubjectList((prev) => {
       const updatedData = [...prev];
       const subject = updatedData[id];
-
-      subjectName = subject.subjectName;
 
       updatedData[id] = {
         ...subject,
@@ -21,18 +14,6 @@ export const useInput = (id: number) => {
       };
       return updatedData;
     });
-
-    setSubjectIncomplete((prev) => ({
-      ...prev,
-      [subjectName]: {
-        ...prev[subjectName],
-        [name === 'achievementLevel21'
-          ? 'isIncomplete21'
-          : name === 'achievementLevel22'
-          ? 'isIncomplete22'
-          : 'isIncomplete31']: isIncomplete,
-      },
-    }));
   };
 
   return { handleSubjectChange };
