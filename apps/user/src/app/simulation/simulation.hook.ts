ca391df598468c9343302156e5f864a2634db6f6
justn-hyeok <justn.hyeok@gmail.com>
2025-08-23 02:21:56 +0900

    feat(user): 성적 입력 미완료 시 alert로 알림

--- apps/user/src/app/simulation/simulation.hook.ts
+++ apps/user/src/app/simulation/simulation.hook.ts
@@ -14,6 +14,26 @@ export const useNavigationClick = () => {
       alert('검정고시 합격자는 기본 점수가 부여돼요.');
       return;
     }
+
+    if (
+      (form.education.graduationType === 'EXPECTED' ||
+        form.education.graduationType === 'GRADUATED') &&
+      currentStep === '성적 입력' &&
+      (step === '출결상황' || step === '봉사시간' || step === '자격증')
+    ) {
+      const hasEmptyAchievementLevel = form.grade.subjectList.some(
+        (subject) =>
+          subject.achievementLevel21 === '-' ||
+          subject.achievementLevel22 === '-' ||
+          subject.achievementLevel31 === '-'
+      );
+
+      if (hasEmptyAchievementLevel) {
+        alert('미입력 값이 있어요. 성적 입력을 먼저 완료해주세요.');
+        return;
+      }
+    }
+
     setCurrentStep(step);
   };
 
