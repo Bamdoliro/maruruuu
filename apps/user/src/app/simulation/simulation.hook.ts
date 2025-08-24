cb49d3dd5bbc7d7d8251c09e8e73e04381bff1fb
justn-hyeok <justn.hyeok@gmail.com>
2025-08-24 21:50:27 +0900

    chore(user): 불필요한 검증 및 alert 삭제

--- apps/user/src/app/simulation/simulation.hook.ts
+++ apps/user/src/app/simulation/simulation.hook.ts
@@ -14,25 +14,6 @@ export const useNavigationClick = () => {
       alert('검정고시 합격자는 기본 점수가 부여돼요.');
       return;
     }
-
-    if (
-      (form.education.graduationType === 'EXPECTED' ||
-        form.education.graduationType === 'GRADUATED') &&
-      currentStep === '성적 입력' &&
-      (step === '출결상황' || step === '봉사시간' || step === '자격증')
-    ) {
-      const hasEmptyAchievementLevel = form.grade.subjectList?.some(
-        (subject) =>
-          subject.achievementLevel21 === '-' ||
-          subject.achievementLevel22 === '-' ||
-          subject.achievementLevel31 === '-'
-      );
-
-      if (hasEmptyAchievementLevel) {
-        alert('성적 미입력 값이 있어요. 성적을 모두 입력해야 정확한 계산이 가능해요.');
-      }
-    }
-
     setCurrentStep(step);
   };
 
