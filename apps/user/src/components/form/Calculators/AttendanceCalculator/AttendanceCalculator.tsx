b677e01003dfe7a4917aac43209d62201ba81a70
sunz8281 <sunz8281@gmail.com>
2025-08-25 22:41:51 +0900

    refactor(user): 하드코딩된 연도를 원서접수 연도로 가져와서 사용

--- apps/user/src/components/form/Calculators/AttendanceCalculator/AttendanceCalculator.tsx
+++ apps/user/src/components/form/Calculators/AttendanceCalculator/AttendanceCalculator.tsx
@@ -6,18 +6,21 @@ import AttendanceCalculatorHeader from './AttendanceCalculatorHeader/AttendanceC
 import { useFormValueStore } from '@/stores';
 import { useInput } from './AttendanceCalculator.hook';
 import AttendanceCalculatorItem from './AttendanceCalculatorItem/AttendanceCalculatorItem';
-import { ATTENDANCE_GRADE } from '@/constants/form/constants';
+import { ATTENDANCE_GRADE, SCHEDULE } from '@/constants/form/constants';
+import { formatFormYear } from '@/utils';
 
 const AttendanceCalculator = () => {
   const form = useFormValueStore();
   const { handleAttendanceInfoChange } = useInput();
 
   const isReadOnly = form.education.graduationType === 'QUALIFICATION_EXAMINATION';
+  const scoreEndYear = SCHEDULE.원서_접수.format('YYYY');
 
   return (
     <StyledAttendanceCalculator>
       <Text fontType="p3" color={color.red}>
-        *2025.09.30까지의 출결상황을 기재해주세요. 졸업생은 졸업일 기준으로 기재해주세요.
+        *{scoreEndYear}.09.30까지의 출결상황을 기재해주세요. 졸업생은 졸업일 기준으로
+        기재해주세요.
       </Text>
       <Column>
         <AttendanceCalculatorHeader />
