e81fe72dc04786dcd0e03fb5e75ef8bb5584007c
sunz8281 <sunz8281@gmail.com>
2025-08-26 16:16:05 +0900

    style(user): 자기소개서 및 학업계획서 placeholder 내용 수정

--- apps/user/src/components/form/Contents/IntroductionContent/IntroductionContent.tsx
+++ apps/user/src/components/form/Contents/IntroductionContent/IntroductionContent.tsx
@@ -20,7 +20,7 @@ const IntroductionContent = () => {
             name="coverLetter"
             limit={1000}
             label="자기소개서"
-            placeholder="1000자 이내로 작성해주세요."
+            placeholder="자유롭게 자신의 특성 및 성장과정, 취미·특기, 학교생활, 가족안에서의 역할, 장점과 보완·발전시켜야할 단점에 대하여 서술하십시오. (1000자 이내)"
             value={form.document.coverLetter}
             onChange={onFieldChange}
             isError={!!errors.coverLetter?.length}
@@ -30,7 +30,7 @@ const IntroductionContent = () => {
             name="statementOfPurpose"
             limit={1000}
             label="학업계획서"
-            placeholder="1000자 이내로 작성해주세요."
+            placeholder="우리학교를 선택하게 된 구체적인 지원동기와 고등학생이 된 후 이루고자 하는 목표를 달성하기 위해 생각하는 학업계획을 기술하십시오. (1000자 이내)"
             value={form.document.statementOfPurpose}
             onChange={onFieldChange}
             isError={!!errors.statementOfPurpose?.length}
