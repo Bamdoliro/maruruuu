a725f34d8429463a5f5d1f245e0d36d9d3acc418
sunz8281 <sunz8281@gmail.com>
2025-09-02 19:56:01 +0900

    feat(admin): 제출 서류 조회기능 개발

--- apps/admin/src/components/form/FormDetail/StatusManager/StatusManager.tsx
+++ apps/admin/src/components/form/FormDetail/StatusManager/StatusManager.tsx
@@ -1,12 +1,14 @@
 import { useOverlay } from '@toss/use-overlay';
 import ReceiveStatusChangeModal from '@/components/form/FormDetail/ReceiveStatusChangeModal/ReceiveStatusChangeModal';
 import { Button, Column } from '@maru/ui';
-
+import { useFormDetailQuery } from '@/services/form/queries';
 interface StatusManagerProps {
   id: number;
 }
 
 const StatusManager = ({ id }: StatusManagerProps) => {
+	const { data: formDetailData } = useFormDetailQuery(id);
+
   const overlay = useOverlay();
 
   const openReceivedStatusChangeModal = () => {
@@ -15,13 +17,20 @@ const StatusManager = ({ id }: StatusManagerProps) => {
     ));
   };
 
+	const handleSubmissionDocumentButtonClick = () => {
+		if (formDetailData?.formUrl) {
+			window.open(formDetailData.formUrl, '_blank');
+		}
+	};
+
   return (
     <Column gap={16}>
       <Column gap={8}>
         <Button styleType="PRIMARY" size="SMALL" onClick={openReceivedStatusChangeModal}>
           최종 접수 상태 변경
         </Button>
-        <Button styleType="SECONDARY" size="SMALL">
+        <Button styleType="SECONDARY" size="SMALL" onClick={handleSubmissionDocumentButtonClick}
+        disabled={!formDetailData?.formUrl}>
           제출 서류 조회
         </Button>
       </Column>
