6cf92f6cc29ce3ef030f5408a22f4b8f33e75479
sunz8281 <sunz8281@gmail.com>
2025-09-03 02:53:08 +0900

    feat(admin): 원서 상세조회 상태 조회 개발

---
+++ apps/admin/src/components/form/FormDetail/StatusManager/StatusManager.hook.ts
@@ -0,0 +1,41 @@
+import { color } from '@maru/design-system';
+import { FormStatus } from '@/types/form/client';
+
+interface StatusInfo {
+  text: string;
+  color: string;
+}
+
+export const useStatusFormatter = () => {
+  const getSubmissionDocumentStatus = (status: FormStatus): StatusInfo => {
+    if (status === 'REJECTED') return { text: '반려', color: color.red };
+    if (status === 'SUBMITTED') return { text: '미제출', color: color.black };
+    if (status === 'FINAL_SUBMITTED') return { text: '제출', color: color.green };
+    if (status === 'RECEIVED') return { text: '접수', color: color.green };
+    if (['APPROVED', 'FIRST_FAILED', 'FAILED', 'NO_SHOW', 'FIRST_PASSED', 'PASSED', 'ENTERED'].includes(status)) {
+      return { text: '승인', color: color.green };
+    }
+    return { text: '미정', color: color.gray600 };
+  };
+
+  const getFirstRoundStatus = (status: FormStatus): StatusInfo => {
+    if (status === 'FIRST_FAILED') return { text: '불합격', color: color.red };
+    if (['FIRST_PASSED', 'FAILED', 'NO_SHOW', 'ENTERED', 'PASSED'].includes(status)) {
+      return { text: '합격', color: color.green };
+    }
+    return { text: '미정', color: color.gray600 };
+  };
+
+  const getSecondRoundStatus = (status: FormStatus): StatusInfo => {
+    if (status === 'FAILED') return { text: '불합격', color: color.red };
+    if (status === 'NO_SHOW') return { text: '불참', color: color.red };
+    if (['PASSED', 'ENTERED'].includes(status)) return { text: '합격', color: color.green };
+    return { text: '미정', color: color.gray600 };
+  };
+
+  return {
+    getSubmissionDocumentStatus,
+    getFirstRoundStatus,
+    getSecondRoundStatus,
+  };
+};
--- apps/admin/src/components/form/FormDetail/StatusManager/StatusManager.tsx
+++ apps/admin/src/components/form/FormDetail/StatusManager/StatusManager.tsx
@@ -1,21 +1,29 @@
-import { useOverlay } from '@toss/use-overlay';
-import ReceiveStatusChangeModal from '@/components/form/FormDetail/ReceiveStatusChangeModal/ReceiveStatusChangeModal';
-import { Button, Column } from '@maru/ui';
+import { Button, Column, Loader, Text } from '@maru/ui';
 import { useFormDetailQuery } from '@/services/form/queries';
+import { styled } from 'styled-components';
+import { color } from '@maru/design-system';
+import { flex } from '@maru/utils';
+import { useStatusFormatter } from '@/components/form/FormDetail/StatusManager/StatusManager.hook';
+import ReceiveStatusChangeModal
+	from '@/components/form/FormDetail/ReceiveStatusChangeModal/ReceiveStatusChangeModal';
+import { useOverlay } from '@toss/use-overlay';
+
 interface StatusManagerProps {
   id: number;
 }
 
 const StatusManager = ({ id }: StatusManagerProps) => {
-	const { data: formDetailData } = useFormDetailQuery(id);
+  const { data: formDetailData } = useFormDetailQuery(id);
+  const { getSubmissionDocumentStatus, getFirstRoundStatus, getSecondRoundStatus } = useStatusFormatter();
+	const overlay = useOverlay();
 
-  const overlay = useOverlay();
+	if (!formDetailData) return <Loader />;
 
-  const openReceivedStatusChangeModal = () => {
-    overlay.open(({ isOpen, close }) => (
-      <ReceiveStatusChangeModal formId={id} isOpen={isOpen} onClose={close} />
-    ));
-  };
+	const openReceivedStatusChangeModal = () => {
+		overlay.open(({ isOpen, close }) => (
+			<ReceiveStatusChangeModal formId={id} isOpen={isOpen} onClose={close} />
+		));
+	};
 
 	const handleSubmissionDocumentButtonClick = () => {
 		if (formDetailData?.formUrl) {
@@ -23,14 +31,40 @@ const StatusManager = ({ id }: StatusManagerProps) => {
 		}
 	};
 
+  const submissionStatus = getSubmissionDocumentStatus(formDetailData?.status);
+  const firstRoundStatus = getFirstRoundStatus(formDetailData?.status);
+  const secondRoundStatus = getSecondRoundStatus(formDetailData?.status);
+
   return (
     <Column gap={16}>
+      <StyledStatusList>
+				<StyledStatusItem>
+					<Text fontType='p2' width={60}>제출 서류</Text>
+					<Text fontType='p2' width={60} color={submissionStatus?.color}>{submissionStatus.text}</Text>
+				</StyledStatusItem>
+	      <StyledStatusItem>
+		      <Text fontType='p2' width={60}>1차 결과</Text>
+		      <Text fontType='p2' width={60} color={firstRoundStatus.color}>{firstRoundStatus.text}</Text>
+	      </StyledStatusItem>
+	      <StyledStatusItem>
+		      <Text fontType='p2' width={60}>최종 점수</Text>
+		      <Text fontType='p2' width={60}>{formDetailData.score.totalScore || "미정"}</Text>
+	      </StyledStatusItem>
+	      <StyledStatusItem>
+		      <Text fontType='p2' width={60}>2차 결과</Text>
+		      <Text fontType='p2' width={60} color={secondRoundStatus.color}>{secondRoundStatus.text}</Text>
+	      </StyledStatusItem>
+      </StyledStatusList>
       <Column gap={8}>
-        <Button styleType="PRIMARY" size="SMALL" onClick={openReceivedStatusChangeModal}>
+        <Button styleType="PRIMARY" size="SMALL" onClick={openReceivedStatusChangeModal} disabled={formDetailData.status === "SUBMITTED"}>
           최종 접수 상태 변경
         </Button>
-        <Button styleType="SECONDARY" size="SMALL" onClick={handleSubmissionDocumentButtonClick}
-        disabled={!formDetailData?.formUrl}>
+        <Button
+          styleType="SECONDARY"
+          size="SMALL"
+          onClick={handleSubmissionDocumentButtonClick}
+          disabled={!formDetailData?.formUrl || formDetailData?.status === "SUBMITTED"}
+        >
           제출 서류 조회
         </Button>
       </Column>
@@ -39,3 +73,22 @@ const StatusManager = ({ id }: StatusManagerProps) => {
 };
 
 export default StatusManager;
+
+const StyledStatusList = styled.div`
+	${flex({ flexDirection: 'column', alignItems: 'center'})}
+	width: 100%;
+  border: 1px solid ${color.gray200};
+  border-radius: 12px;
+	padding: 4px 0;
+`;
+
+const StyledStatusItem = styled.div`
+    ${flex({ flexDirection: 'row', alignItems: 'center' })}
+    gap: 100px;
+    width: 100%;
+    padding: 12px 24px;
+
+    &:not(:last-child) {
+        border-bottom: 1px solid ${color.gray200};
+    }
+`;
