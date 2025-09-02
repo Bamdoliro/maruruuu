128d9c83b264c5c7553649dc591439a04f2fff85
sunz8281 <sunz8281@gmail.com>
2025-09-03 02:48:20 +0900

    fix(admin): 원서 상세조회시 score 타입 추가

--- apps/admin/src/types/form/client.ts
+++ apps/admin/src/types/form/client.ts
@@ -82,6 +82,10 @@ export interface FormDetail {
     coverLetter: string;
     statementOfPurpose: string;
   };
+	score: {
+		firstRoundScore: number;
+		totalScore: number;
+	}
   formUrl: string;
   type: FormType;
   status: FormStatus;
