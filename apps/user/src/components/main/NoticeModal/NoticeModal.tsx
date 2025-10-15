03a548b1790969de3070523c3ad428b9125b7fce
sunz8281 <sunz8281@gmail.com>
2025-10-15 15:41:29 +0900

    fix(user): z index 1에서 1000으로 변경

--- apps/user/src/components/main/NoticeModal/NoticeModal.tsx
+++ apps/user/src/components/main/NoticeModal/NoticeModal.tsx
@@ -78,7 +78,7 @@ const BlurBackground = styled.div<{ $isOpen: boolean }>`
   width: 100%;
   height: 100vh;
   background: rgba(0, 0, 0, 0.4);
-  z-index: 1;
+  z-index: 1000;
 `;
 
 const StyledNoticeModal = styled.div`
