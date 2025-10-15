15fda0c27e5ab22e3d7e281c443addf1656e6e68
hjy080530 <bomaye119@gmail.com>
2025-10-15 17:00:49 +0900

    feat(user): 모달 2개 차례구현

--- apps/user/src/app/page.tsx
+++ apps/user/src/app/page.tsx
@@ -9,18 +9,33 @@ import {
   ScheduleList,
   SimulationBox,
 } from '@/components/main';
-import { AppLayout } from '@/layouts';
-import { Row } from '@maru/ui';
 import { flex } from '@maru/utils';
 import React, { useEffect } from 'react';
 import styled from 'styled-components';
 import { useOverlay } from '@toss/use-overlay';
 import NoticeModal from '@/components/main/NoticeModal/NoticeModal';
+import VolunteerModal from '@/components/main/VolunteerModal/VolunteerModal';
+import { Row } from '@maru/ui';
+import { AppLayout } from '@/layouts';
 
 const Home = () => {
   const overlay = useOverlay();
   useEffect(() => {
-    overlay.open(({ isOpen, close }) => <NoticeModal isOpen={isOpen} onClose={close} />);
+    const openNoticeModal = () => {
+      overlay.open(({ isOpen, close }) => (
+        <NoticeModal isOpen={isOpen} onClose={close} />
+      ));
+    };
+
+    overlay.open(({ isOpen, close }) => (
+      <VolunteerModal
+        isOpen={isOpen}
+        onClose={() => {
+          close();
+          openNoticeModal();
+        }}
+      />
+    ));
   }, [overlay]);
 
   return (
