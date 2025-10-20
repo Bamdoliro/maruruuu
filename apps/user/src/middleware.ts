baee843fce7c1f2a8fd33698757d5ac283dd748b
junghongseop <a01082372487@gmail.com>
2025-10-20 11:47:19 +0900

    refactor(user): update import statements in middleware

--- apps/user/src/middleware.ts
+++ apps/user/src/middleware.ts
@@ -1,4 +1,5 @@
-import { NextRequest, NextResponse } from 'next/server';
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
 
