1930d29271242d63c0dd8d3a55351a02bef470ea
hjy080530 <bomaye119@gmail.com>
2025-10-20 17:11:07 +0900

    feat(user): 검정고시 출결 삭제

--- apps/user/src/app/form/Grade/Score/Score.hook.ts
+++ apps/user/src/app/form/Grade/Score/Score.hook.ts
@@ -57,7 +57,11 @@ export const useCTAButton = () => {
 
   const handleNextStep = () => {
     if (validateSubjects()) {
-      setFormGradeStep('출결상황');
+      if (form.education.graduationType === 'QUALIFICATION_EXAMINATION') {
+        setFormGradeStep('봉사시간');
+      } else {
+        setFormGradeStep('출결상황');
+      }
       saveFormMutate(form);
     }
   };
