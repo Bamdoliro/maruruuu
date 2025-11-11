20404e530edfa2968995a7a68ecf2ebce3510e77
sunz8281 <sunz8281@gmail.com>
2025-11-11 22:18:12 +0900

    chore(user): prettier 적용

--- apps/user/src/components/result/ResultBox/first/FirstResultContent/PassBox/PassBox.tsx
+++ apps/user/src/components/result/ResultBox/first/FirstResultContent/PassBox/PassBox.tsx
@@ -17,8 +17,8 @@ const PassBox = () => {
     <StyledPassBox>
       <Column gap={24} alignItems="center">
         <Text fontType="p1" color={color.gray900} textAlign="center">
-          2단계 전형 응시를 위해 수험표를 출력하고 {formatMonthDay(SCHEDULE.이차_면접)}에 본교에 방문해주시기
-          바랍니다.
+          2단계 전형 응시를 위해 수험표를 출력하고 {formatMonthDay(SCHEDULE.이차_면접)}에
+          본교에 방문해주시기 바랍니다.
           <br />
           자세한 내용은 입학 요강에서 확인해주시기 바랍니다.
         </Text>
