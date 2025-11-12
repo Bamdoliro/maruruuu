eb8f0cc3c02f3cd97814f21f460672e856ca39bb
sunz8281 <sunz8281@gmail.com>
2025-11-12 09:59:22 +0900

    chore(user): prettier 적용

--- apps/user/src/middleware.ts
+++ apps/user/src/middleware.ts
@@ -9,16 +9,22 @@ dayjs.extend(isBetween);
 export const middleware = (request: NextRequest) => {
   const now = dayjs();
 
-  const inspectionStart = SCHEDULE.점검_시작
-  const inspectionEnd = SCHEDULE.점검_끝
+  const inspectionStart = SCHEDULE.점검_시작;
+  const inspectionEnd = SCHEDULE.점검_끝;
 
   const inspectionUrl = new URL('/inspection', request.url);
 
-  if (now.isBetween(inspectionStart, inspectionEnd) && request.nextUrl.pathname !== '/inspection') {
+  if (
+    now.isBetween(inspectionStart, inspectionEnd) &&
+    request.nextUrl.pathname !== '/inspection'
+  ) {
     return NextResponse.redirect(inspectionUrl);
   }
 
-  if (!now.isBetween(inspectionStart, inspectionEnd) && request.nextUrl.pathname === '/inspection') {
+  if (
+    !now.isBetween(inspectionStart, inspectionEnd) &&
+    request.nextUrl.pathname === '/inspection'
+  ) {
     return NextResponse.redirect(new URL('/', request.url));
   }
 };
