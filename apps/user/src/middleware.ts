3baaeeb28bb799744e5521b6171ff113fd72000b
hjy080530 <bomaye119@gmail.com>
2025-11-12 10:50:54 +0900

    Revert "feat(user): 미들웨어 matcher 추가"
    This reverts commit aae74a568f39b3f4a8792e8bbe92284836331551.

--- apps/user/src/middleware.ts
+++ apps/user/src/middleware.ts
@@ -2,33 +2,22 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 import dayjs from 'dayjs';
 import isBetween from 'dayjs/plugin/isBetween';
-import { SCHEDULE } from '@/constants/form/constants';
-import { ROUTES } from '@/constants/common/constants';
 
 dayjs.extend(isBetween);
 
 export const middleware = (request: NextRequest) => {
   const now = dayjs();
 
-  if (
-    now.isBetween(SCHEDULE.점검_시작, SCHEDULE.점검_끝) &&
-    request.nextUrl.pathname !== ROUTES.INSPECTION
-  ) {
-    return NextResponse.redirect(new URL(ROUTES.INSPECTION, request.url));
+  const 점검_시작 = dayjs(process.env.NEXT_PUBLIC_INSPECTION_START_DAY);
+  const 점검_끝 = dayjs(process.env.NEXT_PUBLIC_INSPECTION_END_DAY);
+
+  const inspectionUrl = new URL('/inspection', request.url);
+
+  if (now.isBetween(점검_시작, 점검_끝) && request.nextUrl.pathname !== '/inspection') {
+    return NextResponse.redirect(inspectionUrl);
   }
 
-  if (
-    !now.isBetween(SCHEDULE.점검_시작, SCHEDULE.점검_끝) &&
-    request.nextUrl.pathname === ROUTES.INSPECTION
-  ) {
-    return NextResponse.redirect(new URL(ROUTES.MAIN, request.url));
+  if (!now.isBetween(점검_시작, 점검_끝) && request.nextUrl.pathname === '/inspection') {
+    return NextResponse.redirect(new URL('/', request.url));
   }
-
-  return NextResponse.next();
-};
-
-export const config = {
-  matcher: [
-    '/((?!api|_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
-  ],
 };
