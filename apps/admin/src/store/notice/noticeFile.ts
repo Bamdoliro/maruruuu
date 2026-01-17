85ba0daa678c819d40194098480264afe566d304
hjy080530 <bomaye119@gmail.com>
2026-01-17 23:31:58 +0900

    refactor(admin): recoil 세분화

--- apps/admin/src/store/notice/noticeFile.ts
+++ apps/admin/src/store/notice/noticeFile.ts
@@ -1,4 +1,4 @@
-import { atom, useRecoilState } from 'recoil';
+import { atom, useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil';
 
 const noticeFileAtomState = atom<File[] | null>({
   key: 'notice-file',
@@ -10,7 +10,13 @@ const uploadedNoticeFileAtomState = atom<File | null>({
   default: null,
 });
 
+export const useNoticeFileStore = () => useRecoilState(noticeFileAtomState);
+export const useNoticeFileValueStore = () => useRecoilValue(noticeFileAtomState);
+export const useSetNoticeFileStore = () => useSetRecoilState(noticeFileAtomState);
+
 export const useUploadedNoticeFileStore = () =>
   useRecoilState(uploadedNoticeFileAtomState);
-
-export const useNoticeFileStore = () => useRecoilState(noticeFileAtomState);
+export const useUploadedNoticeFileValueStore = () =>
+  useRecoilValue(uploadedNoticeFileAtomState);
+export const useSetUploadedNoticeFileStore = () =>
+  useSetRecoilState(uploadedNoticeFileAtomState);
